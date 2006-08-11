f91c11b0c0d041fa44c982cc2a26a225046a37b3
lakin <lakin@503f2841-2733-4260-b5db-97e3c2726e36>
2006-08-11 14:28:49 +0000

    * Importing the latest version from lakin's bzr repository.
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@1 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python-ogre/ogre_customization_data.py
@@ -0,0 +1,16 @@
+name2alias = {
+    "map<std::string, bool, std::less<std::string>, std::allocator<std::pair<std::string const, bool> > >":
+        "MapOfStringToBool",
+    "map<std::string, Ogre::OverlayElement*, std::less<std::string>, std::allocator<std::pair<std::string const, Ogre::OverlayElement*> > >":
+        "MapOfStringToOverlayElement",
+    "map<std::string, Ogre::MovableObject*, std::less<std::string>, std::allocator<std::pair<std::string const, Ogre::MovableObject*> > >":
+        "MapOfStringToMovableObject",
+    "map<std::string, std::string, std::less<std::string>, std::allocator<std::pair<std::string const, std::string> > >":
+        "MapOfStringToString",
+    "vector<float, std::allocator<float> >":
+        "VectorOfFloat",
+    "vector<std::string, std::allocator<std::string> >":
+        "VectorOfString",
+}
+# vim:et:ts=4:sts=4:sw=4
+
