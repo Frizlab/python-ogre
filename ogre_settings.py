21cb20e4ae927ec40a15989fc63a84a3e8b9cb40
roman <roman@503f2841-2733-4260-b5db-97e3c2726e36>
2006-08-13 08:15:13 +0000

    adding my ( Roman ) settings
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@4 503f2841-2733-4260-b5db-97e3c2726e36

--- ogre_settings.py
+++ ogre_settings.py
@@ -1,18 +1,27 @@
 import os
 import sys
+import getpass
 
-gccxml_path = r'c:/development/gccxml/bin/release/gccxml.exe'
-pygccxml_base_dir = r'c:\development\pyplus'
-working_dir = r'c:\development\python-ogre-r'
-headers_dir = r'c:\development\ogresdk7\include'
 
-python_headers_dir = 'c:/python25/include'
+
+if 'roman' in getpass.getuser():
+    gccxml_path = r'd:/gccxml_cvs/gccxml-build/bin/release/gccxml.exe'
+    pygccxml_base_dir = r'D:\pygccxml_sources\sources'
+    working_dir = r'D:\python-ogre'
+    headers_dir = r'D:\ogre\ogrenew\OgreMain\include'
+    python_headers_dir = 'c:/python/include'
+else:
+    gccxml_path = r'c:/development/gccxml/bin/release/gccxml.exe'
+    pygccxml_base_dir = r'c:\development\pyplus'
+    working_dir = r'c:\development\python-ogre-r'
+    headers_dir = r'c:\development\ogresdk7\include'
+    python_headers_dir = 'c:/python25/include'
+
 
 sys.path.append (pygccxml_base_dir + r'/pygccxml_dev')
 sys.path.append (pygccxml_base_dir + r'/pyplusplus_dev')
 
-
-cache_dir = working_dir + r'\xml_cache'
+cache_dir = working_dir + r'/xml_cache'
 build_dir = working_dir + r'/build'
 decl_cache_dir = working_dir + r'/decl_cache'
 
