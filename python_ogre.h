690cfdc4ee125501efdb5187fddf1e1eba00922c
roman <roman@503f2841-2733-4260-b5db-97e3c2726e36>
2006-08-17 07:02:24 +0000

    adding python_ogre.h header file. It will allow to improve generation script performance
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@18 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python_ogre.h
@@ -0,0 +1,19 @@
+#ifndef __python_ogre_h_5B402FBF_8DE8_49C9_AF71_1CF9634B5344__
+#define __python_ogre_h_5B402FBF_8DE8_49C9_AF71_1CF9634B5344__
+
+//See best practices section in Py++ documentation
+
+#include "Ogre.h"
+
+namespace python_ogre{ namespace details{
+
+inline void instantiate(){
+    using namespace Ogre;
+
+    sizeof( Controller<float> );
+}
+
+} } //python_ogre::details
+
+
+#endif//__python_ogre_h_5B402FBF_8DE8_49C9_AF71_1CF9634B5344__
