f46cd039a7505f88af4f5c835195e978d1a62fda
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2006-11-10 04:05:17 +0000

    Moved most of the sample framework into the Ogre module (sf, sf_ois are in the Orge package dir etc) to simplify maintenance.  Added initial version of Newton code generator - compiles and loads (no testing yet). Another cegui demo. Updated various packages 'inits' etc
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@85 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/demos/CEGUI/SampleFramework.py
+++ python-ogre/demos/CEGUI/SampleFramework.py
@@ -4,14 +4,15 @@
 #
 
 import Ogre as ogre
+
 try:
     ogre.OgreVersion = ogre.GetOgreVersion()
     ogre.OgreVersionString = ogre.OgreVersion[0] + ogre.OgreVersion[1] + ogre.OgreVersion[2]
     ogre.PythonOgreVersion = ogre.GetPythonOgreVersion()
     if (ogre.OgreVersion[0]+ ogre.OgreVersion[1]) == "12":   # ogre 1.2.3 is latest SDK
-        from sf import *
+        from Ogre.sf import *
     else:  # assume it's ogre version 1.3, the CVS version that needs OIS
-        from sf_OIS import * 
+        from Ogre.sf_OIS import * 
            
 except:
     print "Your version of Python-Ogre doesn't define it's own version and so is rather old!"
---
+++ python-ogre/packages/CEGUI/__init__.py
@@ -0,0 +1,1 @@
+from _CEGUI_ import *
