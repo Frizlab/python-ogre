46f0d4eb13157d4671ed0d391c72caa3494585cb
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2006-11-10 04:05:17 +0000

    Moved most of the sample framework into the Ogre module (sf, sf_ois are in the Orge package dir etc) to simplify maintenance.  Added initial version of Newton code generator - compiles and loads (no testing yet). Another cegui demo. Updated various packages 'inits' etc
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@85 503f2841-2733-4260-b5db-97e3c2726e36

--- packages/Ogre/__init__.py
+++ packages/Ogre/__init__.py
@@ -1,5 +1,5 @@
 from _ogre_ import *
-# note a hack to get the ogre version - will expose version directly to python in future release
-version = "CVS"
-#version = "1.2.3"
+OgreException = _ogre_.Exception
 
+
+    
