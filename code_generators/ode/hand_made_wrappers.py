af2dced8b74087bf4f78fd3ebc8ac7eaf7e6aca0
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2006-11-12 18:13:08 +0000

    Added OgreNewt (Newton) support, first 2 demos working, call backs to be implemented.  Further updates to Ode support, a couple more wrappers still required.  Note that you need the media files from OgreNewt CVS for the demos (at present)
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@86 503f2841-2733-4260-b5db-97e3c2726e36

--- code_generators/ode/hand_made_wrappers.py
+++ code_generators/ode/hand_made_wrappers.py
@@ -1,5 +1,93 @@
 import os
 import environment
 
+WRAPPER_DEFINITION_dBody=\
+"""
+::dReal
+dBody_getPosRelPoint ( ::dBody & body, ::dReal x,::dReal y,::dReal z ) {
+    static ::dReal returnValue;
+    body.getPosRelPoint( x, y, z, &returnValue);
+    return returnValue;
+}
+
+::dReal
+dBody_getRelPointPos ( ::dBody & body, ::dReal x,::dReal y,::dReal z ) {
+    static ::dReal returnValue;
+    body.getRelPointPos( x, y, z, &returnValue);
+    return returnValue;
+}
+
+::dReal
+dBody_getPointVel ( ::dBody & body, ::dReal x,::dReal y,::dReal z ) {
+    static ::dReal returnValue;
+    body.getPointVel( x, y, z, &returnValue);
+    return returnValue;
+}
+
+::dReal
+dBody_vectorFromWorld ( ::dBody & body, ::dReal x,::dReal y,::dReal z ) {
+    static ::dReal returnValue;
+    body.vectorFromWorld( x, y, z, &returnValue);
+    return returnValue;
+}
+
+::dReal
+dBody_vectorToWorld ( ::dBody & body, ::dReal x,::dReal y,::dReal z ) {
+    static ::dReal returnValue;
+    body.vectorToWorld( x, y, z, &returnValue);
+    return returnValue;
+}
+::dReal
+dBody_getRelPointVel ( ::dBody & body, ::dReal x,::dReal y,::dReal z ) {
+    static ::dReal returnValue;
+    body.getRelPointVel( x, y, z, &returnValue);
+    return returnValue;
+}
+        
+::dReal
+dBody_getFiniteRotationAxis ( ::dBody & body ) {
+    static ::dReal returnValue;
+    body.getFiniteRotationAxis( &returnValue);
+    return returnValue;
+}
+"""        
+            
+# WRAPPER_DEFINITION_RenderWindow = \
+# """
+# boost::python::tuple 
+# RenderWindow_getMetricsIntWrapper( Ogre::RenderWindow& rw ) {
+#    unsigned int width, height, colourDepth;
+#    int left, top;
+#    rw.getMetrics(  width,  height, colourDepth,  left,  top );
+#    return ( boost::python::make_tuple(width, height, colourDepth, left, top));
+# }
+# """
+
+
+WRAPPER_REGISTRATION_dBody = \
+"""
+    def( "getPosRelPoint", &::dBody_getPosRelPoint );
+    dBody_exposer.def( "getRelPointPos", &::dBody_getRelPointPos );
+    dBody_exposer.def( "getPointVel", &::dBody_getPointVel );
+    dBody_exposer.def( "getRelPointVel", &::dBody_getRelPointVel );
+    dBody_exposer.def( "getFiniteRotationAxis", &::dBody_getFiniteRotationAxis );
+    dBody_exposer.def( "vectorFromWorld", &::dBody_vectorFromWorld );
+    dBody_exposer.def( "vectorToWorld", &::dBody_vectorToWorld );
+"""
+
+# WRAPPER_REGISTRATION_General =\
+# """
+# bp::def ("CreateBody", &CreateBody, 
+#         bp::return_value_policy< bp::reference_existing_object, bp::default_call_policies >());
+# # bp::def ("CreateBodyWithID", &CreateBodyWithID, 
+# #         bp::return_value_policy< bp::reference_existing_object, bp::default_call_policies >());
+# # bp::def ("GetID", &GetID, 
+# #         bp::return_value_policy< bp::copy_const_reference, bp::default_call_policies >());
+# """
 def apply( mb ):
-    pass    
+#     pass
+    cs = mb.class_( 'dBody' )
+
+    cs.add_declaration_code( WRAPPER_DEFINITION_dBody )
+    cs.add_registration_code( WRAPPER_REGISTRATION_dBody )
+    
---
+++ code_generators/ogrenewt/python_ogrenewt.h
@@ -0,0 +1,20 @@
+#ifndef __python_ogrenewt_h_01
+#define __python_ogrenewt_h_01
+
+//See best practices section in Py++ documentation
+
+#include <vector>
+#include <string>
+#include <map>
+
+#include "ogrenewt.h"
+
+//namespace python_ogrenewt{ 
+
+    inline void instantiate(){
+                
+    }
+
+//}
+
+#endif
