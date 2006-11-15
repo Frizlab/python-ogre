4da627800a41c70454564bf488377ff569e89093
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2006-11-15 07:54:15 +0000

    Updates to OgreNewt support along with an additional demo (1-3 now done)
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@87 503f2841-2733-4260-b5db-97e3c2726e36

--- code_generators/ogrenewt/hand_made_wrappers.py
+++ code_generators/ogrenewt/hand_made_wrappers.py
@@ -1,17 +1,50 @@
 import os
 import environment
 
-WRAPPER_DEFINITION_General=\
+WRAPPER_DEFINITION_Body=\
+"""
+void
+Body_setUserData ( ::OgreNewt::Body & body, PyObject * data ) {
+    body.setUserData ( data );
+    }
+PyObject *
+Body_getUserData ( ::OgreNewt::Body & body) {
+    void *  data = body.getUserData (  );
+    Py_INCREF( (PyObject *) data );     // I'm passing a reference to this object so better inc the ref :)
+    return  (PyObject *) data;
+    }
+"""
+WRAPPER_REGISTRATION_Body =\
+"""
+def ("setUserData", &::Body_setUserData );
+Body_exposer.def ("getUserData", &::Body_getUserData);
+"""
+WRAPPER_DEFINITION_Joint=\
+"""
+void
+Joint_setUserData ( ::OgreNewt::Joint & joint, PyObject * data ) {
+    joint.setUserData ( data );
+    }
+PyObject *
+Joint_getUserData ( ::OgreNewt::Joint & joint) {
+    void *  data = joint.getUserData (  );
+    Py_INCREF( (PyObject *) data );     // I'm passing a reference to this object so better inc the ref :)
+    return  (PyObject *) data;
+    }
+"""
+WRAPPER_REGISTRATION_Joint =\
+"""
+def ("setUserData", &::Joint_setUserData );
+Joint_exposer.def ("getUserData", &::Joint_getUserData);
 """
 
-"""
-WRAPPER_REGISTRATION_General =\
-"""
 
-"""
 def apply( mb ):
-    pass
-#     
-#     mb.add_declaration_code( WRAPPER_DEFINITION_General )
-#     mb.add_registration_code( WRAPPER_REGISTRATION_General )
-    
+    cs = mb.class_( 'Body' )
+    cs.add_declaration_code( WRAPPER_DEFINITION_Body )
+    cs.add_registration_code( WRAPPER_REGISTRATION_Body )
+
+    cs = mb.class_( 'Joint' )
+    cs.add_declaration_code( WRAPPER_DEFINITION_Joint )
+    cs.add_registration_code( WRAPPER_REGISTRATION_Joint )
+        
