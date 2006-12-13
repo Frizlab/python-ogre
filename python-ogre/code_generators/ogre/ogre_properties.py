9cccc64394a47f4dbfe97768359f0f978ee04724
roman <roman@503f2841-2733-4260-b5db-97e3c2726e36>
2006-12-13 08:20:38 +0000

    Py++ improved its interface, so now this class could be simpler
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@104 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/code_generators/ogre/ogre_properties.py
+++ python-ogre/code_generators/ogre/ogre_properties.py
@@ -11,37 +11,19 @@ class ogre_property_recognizer_t( decl_wrappers.name_based_recognizer_t ):
     def __init__( self ):
         decl_wrappers.name_based_recognizer_t.__init__( self )    
     
-    def __is_setter( self, mem_fun ):
-        if len( mem_fun.arguments ) != 1:
-            return False
-        if not declarations.is_void( mem_fun.return_type ):
-            return False
-        if mem_fun.has_const:
-            return False
-        return True
-
     def create_read_only_property( self, fget ):
         found = self.find_out_ro_prefixes( fget.name )
         if found in ( None, '' ):
             return None
-            
+
         pure_name = fget.name[len( found ):]
         pure_name = pure_name.lower()
-        is_setter = lambda decl: decl.name.lower() == pure_name and self.__is_setter( decl )
-        setters = fget.parent.mem_funs( is_setter, allow_empty=True, recursive=False )
-        print "1",setters
-        for hierarchy_info in fget.parent.recursive_bases:
-            if hierarchy_info.related_class.ignore: #don't scan excluded classes
-                continue
-            if 'public' != hierarchy_info.access_type: #don't scan non public hierarchy
-                continue
-            base_cls = hierarchy_info.related_class
-            print "2",setters
-            try:
-                setters.extend( base_cls.mem_funs( is_setter, recursive=False  , allow_empty=True ) )
-            except:
-                print "Exception !!!!!!!!"
+
+        setters = self.class_accessors( fget.parent )[1] 
+        setters.extend( self.inherited_accessors( fget.parent )[1] )
         
+        setters = filter( lambda decl: decl.name.lower() == pure_name and self.is_setter( decl )
+                          , setters )
         if setters:
             #we found member function that could serve as setter. Py++ did not find it
             #so there is a difference in cases.
