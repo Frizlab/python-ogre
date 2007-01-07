80b7b363146453450235b26aa1b20d4b7255d19a
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-01-07 13:19:51 +0000

    Fairly significant changes to how "sizeof's" and "aliases" are managed - now in separate .h files. remember that changes to these require the xml cache files to be deleted before running generate_code.py.... There may be numerous things broken as testing has been minimal :(
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@123 503f2841-2733-4260-b5db-97e3c2726e36

--- code_generators/ogre/python_ogre.h
+++ code_generators/ogre/python_ogre.h
@@ -24,6 +24,37 @@
 #include "OgreSkeleton.h"
 #include "OgreCompositor.h"
 
+//#ifdef OGRE_VERSION_CVS
+
+
+namespace pyplusplus { namespace aliases{
+ using namespace Ogre;
+
+ #include "python_ogre_alias.h"
+} }
+
+
+namespace python_ogre{ namespace details{
+inline void instantiate(){
+ using namespace Ogre;
+ #include "python_ogre_sizeof.h"
+ 
+ sizeof ( Controller<float> );
+ // note these std's need to be specifically 'included' in generate_code...
+ sizeof (std::pair<bool, float> );
+ sizeof (std::pair<unsigned int,unsigned int> );
+ sizeof (std::map<std::string, std::string, std::less<std::string>, std::allocator<std::pair<std::string const, std::string> > >);
+ sizeof (ConstMapIterator<std::map<std::string, std::string, std::less<std::string>, std::allocator<std::pair<std::string const, std::string> > > >); 
+ sizeof (stdext::_Hash<stdext::_Hmap_traits<std::string, unsigned short, stdext::hash_compare<std::string, std::less<std::string> >, std::allocator<std::pair<std::string const, unsigned short> >, false> >);
+ sizeof (stdext::hash_compare<std::string, std::less<std::string> >);
+ sizeof(stdext::hash_map<std::string, unsigned short, stdext::hash_compare<std::string, std::less<std::string> >, std::allocator<std::pair<std::string const, unsigned short> > >);
+ sizeof(HashMap<String, ushort>);
+ sizeof( Ogre::Mesh::SubMeshNameMap );
+ 
+} } }
+
+//#endif
+
 
 
 #ifndef OGRE_VERSION_CVS        // these are needed for the 1.2.x SDK
@@ -64,220 +95,6 @@ public:
 }
 #endif
 
-namespace python_ogre{ namespace details{
-
-inline void instantiate(){
-    using namespace Ogre;
-    
-    sizeof (AlignedAllocator<Vector4, 0>  );
-    sizeof (AnimableValue );
-    sizeof (AnimableValuePtr );
-    sizeof (ConstMapIterator<std::map<std::string, std::string > > );
-    sizeof (Controller<float> );
-    sizeof (MapIterator<std::map<short unsigned int, NodeAnimationTrack*> >);
-    sizeof (MapIterator<std::map<short unsigned int, NumericAnimationTrack*> >);
-    sizeof (MapIterator<std::map<std::string, MovableObjectFactory*> >);
-    sizeof (MapIterator<std::map<std::string, Overlay*> >);
-    sizeof (MapIterator<std::map<std::string, ParticleAffectorFactory*> >);
-    sizeof (MapIterator<std::map<std::string, std::string > >);
-    sizeof (MapIterator<std::map<std::string, std::multimap<std::string, std::string >* > >);
-    sizeof (MapIterator<std::map<std::string, MovableObject*> >);
-    sizeof (MapIterator<std::map<std::string, OverlayContainer*> >);
-    sizeof (MapIterator<std::map<std::string, OverlayContainer*> >);
-    sizeof (MapIterator<std::map<std::string, std::multimap<std::string, std::string> > >);
-    sizeof (MapIterator<std::map<unsigned char, RenderQueueGroup*> >);
-    sizeof (MapIterator<std::map<unsigned short, NodeAnimationTrack*> >);
-    sizeof (MapIterator<std::map<unsigned short, NumericAnimationTrack*> >);
-    sizeof (MapIterator<std::map<unsigned short, VertexAnimationTrack*> >);
-    sizeof (MapIterator<std::multimap<std::string, std::string > >);
-    sizeof (MapIterator<stdext::hash_map<std::string, MovableObject* > >);
-    sizeof (MapIterator<stdext::hash_map<std::string, Node* > >); 
-        
-    // sizeof (ArchiveFactory );  // AJM this doesn't expose well :)
-    sizeof (CompositorInstance::RenderSystemOperation );
-    sizeof (ConstMapIterator<std::map<std::string, AnimationState*> >);
-    sizeof (ConstMapIterator<std::map<std::string, MovableObjectFactory*> >);
-    sizeof (ConstMapIterator<stdext::hash_map<std::string, MovableObject*> >);
-    sizeof (ConstMapIterator<std::map<unsigned int, Vector3> >);
-    sizeof (ConstVectorIterator<std::list<AnimationState*> >);
-    sizeof (ConstVectorIterator<std::vector<const SceneManagerMetaData*> >);
-    sizeof (ConstVectorIterator<std::vector<float> >);
-    sizeof (ConstVectorIterator<std::vector<GpuProgramParameters::AutoConstantEntry> >);
-    sizeof (ConstVectorIterator<std::vector<GpuProgramParameters::IntConstantEntry> >);
-    sizeof (ConstVectorIterator<std::vector<GpuProgramParameters::RealConstantEntry> >);
-    sizeof (ConstVectorIterator<std::vector<LinkedSkeletonAnimationSource> >);
-    sizeof (ConstVectorIterator<std::vector<Node*> >);
-    sizeof (ConstVectorIterator<std::vector<Pose*> >);
-    sizeof (ConstVectorIterator<std::vector<ShadowTextureConfig> >);
-    sizeof (ConstVectorIterator<std::vector<TextureUnitState*> >);
-    sizeof (ConstVectorIterator<std::vector<VertexPoseKeyFrame::PoseRef> >);
-    sizeof (DataStreamPtr );
-    sizeof (GpuProgramParameters::AutoConstantEntry );
-    sizeof (HardwareIndexBuffer );
-    sizeof (HardwareIndexBufferSharedPtr );  
-    sizeof (HardwarePixelBufferSharedPtr );
-    sizeof (MapIterator<std::map<std::string, OverlayContainer*> >);
-    sizeof (MapIterator<std::map<std::string, ParticleEmitterFactory*> >);
-    sizeof (MapIterator<std::map<std::string, ParticleSystemRendererFactory*> >);
-    sizeof (MapIterator<std::map<std::string, SceneManager*> >);     
-    sizeof (MapIterator<std::map<std::string, Animation*> >);
-    sizeof (MapIterator<std::map<std::string, Camera*> >);
-    sizeof (MapIterator<std::map<std::string, MovableObject*> >);
-    sizeof (MapIterator<std::map<std::string, OverlayElement*> >);
-    sizeof (MapIterator<std::map<std::string, ParticleSystem*> >);
-    sizeof (MapIterator<std::map<std::string, RenderTarget*> >);
-    sizeof (MapIterator<std::map<std::string, ResourceManager*> >);
-    sizeof (MapIterator<std::map<std::string, std::multimap<std::string, std::string> > >);
-    sizeof (MapIterator<std::map<unsigned char, RenderQueueGroup*> >);
-    sizeof (MapIterator<std::map<unsigned int, StaticGeometry::Region*> >);
-    sizeof (MapIterator<std::map<unsigned int, Vector3> >);
-    sizeof (MapIterator<std::map<unsigned long, SharedPtr<Resource> > >);
-    sizeof (MapIterator<std::map<unsigned short, RenderPriorityGroup*> >);
-    sizeof (MapIterator<std::multimap<std::string, std::string> >);
-    sizeof (MapIterator<std::multimap<unsigned int, VertexBoneAssignment_s> >);
-    sizeof (MapIterator<std::multimap<unsigned, VertexBoneAssignment_s> >);
-    sizeof (MapIterator<stdext::hash_map<std::string, MovableObject*> >);
-    sizeof (MapIterator<stdext::hash_map<std::string, MovableObject*> >);
-    sizeof (MapIterator<std::multimap<unsigned, VertexBoneAssignment_s> >);
-    sizeof (MapIterator<std::map<std::string, Ogre::StaticGeometry::MaterialBucket* > >);
-    sizeof (MaterialPtr );
-    sizeof (MaterialPtr );
-    sizeof (MeshPtr );
-    
-    sizeof (OverlayElementFactory );
-    sizeof (ParticleAffectorFactory );
-    sizeof (ParticleEmitterFactory );
-    sizeof (ParticleSystemRendererFactory );
-    sizeof (PatchMeshPtr );
-//    sizeof (PositionTarget  );
-    sizeof (SharedPtr<ControllerValue<float> > );
-    sizeof (SharedPtr<DataStream> );
-    sizeof (SharedPtr<std::vector<std::string> >);
-    sizeof (SharedPtr<std::vector<std::string> >);
-    sizeof (SharedPtr<std::vector<std::string> >);
-    
-    // these two require a try/except block in shared pointer creation..
-//     sizeof (SharedPtr<std::vector<FileInfo> >);
-//     sizeof (SharedPtr<std::list<SharedPtr<DataStream> > >);
-
- 
-     sizeof (Singleton<ArchiveManager> );
-    sizeof (StringVector );
-    sizeof (StringVectorPtr );
-    sizeof (TexturePtr );
-    sizeof (DataStreamPtr);
-    sizeof (VectorIterator<std::vector<StaticGeometry::LODBucket *> >);
-    sizeof (VectorIterator<std::vector<Bone*> >);
-    sizeof (VectorIterator<std::vector<CompositionPass*> >);
-    sizeof (VectorIterator<std::vector<CompositionTechnique*> >);
-    sizeof (VectorIterator<std::vector<CompositionTechnique::TextureDefinition*> >);
-    sizeof (VectorIterator<std::vector<Pose*> >);
-    sizeof (VectorIterator<std::vector<ShadowRenderable*> >);
-    sizeof (VectorIterator<std::vector<StaticGeometry::GeometryBucket*> >);
-    sizeof (VectorIterator<std::vector<Technique*> >);
-    sizeof (VectorIterator<std::vector<VertexPoseKeyFrame::PoseRef> >);
-    sizeof (VertexBoneAssignment_s );   
-    sizeof (RadixSort<std::list<Billboard*>, Ogre::Billboard*, float >);
-    sizeof (RadixSort<std::list<Particle*>, Ogre::Particle*, float >);
-    sizeof (RadixSort<std::vector<RenderablePass>, Ogre::RenderablePass, float >);
-    sizeof (RadixSort<std::vector<RenderablePass>, Ogre::RenderablePass, unsigned int >);
-    sizeof (SharedPtr< Controller<float> >);
-    sizeof (SharedPtr< std::vector<String> > );
-    sizeof (SharedPtr< WaveformControllerFunction > );
-    sizeof (SharedPtr<AnimableValue>  );
-    sizeof (SharedPtr<ControllerFunctionRealPtr> );
-    sizeof (SharedPtr<ControllerValueRealPtr> );
-    sizeof (SharedPtr<HardwareIndexBuffer> );
-    sizeof (SharedPtr<MemoryDataStream>);
-    sizeof (SharedPtr<Resource> );
-    sizeof (SharedPtr<StringVector> );
-    sizeof (SharedPtr<StringVectorPtr> );
-    sizeof (SharedPtr<std::vector<std::string> >);
-    sizeof (Singleton<ArchiveManager>);
-    sizeof (Singleton<CompositorManager>);
-    sizeof (Singleton<ControllerManager>);
-    sizeof (Singleton<GpuProgramManager>);
-    sizeof (Singleton<HardwareBufferManager>);
-    sizeof (Singleton<HighLevelGpuProgramManager>);
-    sizeof (Singleton<LogManager>);
-    sizeof (Singleton<MaterialManager>);
-    sizeof (Singleton<MeshManager>);
-    sizeof (Singleton<OverlayManager>);
-    sizeof (Singleton<ParticleSystemManager>);
-    sizeof (Singleton<Profiler>);
-    sizeof (Singleton<ResourceBackgroundQueue>);
-    sizeof (Singleton<ResourceGroupManager>);
-    sizeof (Singleton<Root>);
-    sizeof (Singleton<SceneManagerEnumerator>);
-    sizeof (Singleton<ShadowTextureManager>);
-    sizeof (Singleton<SkeletonManager>);
-    sizeof (Singleton<TextureManager>);
-    sizeof (std::list<AnimationState*>);
-    sizeof (std::list<AnimationState*>::iterator);
-    sizeof (std::list<AnimationState*>::const_iterator );
-    sizeof (std::map<std::string, AnimationState*>);
-    sizeof (std::map<std::string, std::string > );
-    sizeof (std::map<std::string, MovableObject*> );
-    sizeof (std::map<String, String> );    // NameValuePairList
-    sizeof (std::map<unsigned int, StaticGeometry::Region *> );
-    sizeof (std::map<unsigned, Vector3>);
-    sizeof (std::multimap<RenderWindow*,WindowEventListener*>);
-    sizeof (std::multimap<Vector3,Vector3>);
-    sizeof (std::multimap<RenderWindow*, WindowEventListener*> ) ; // WindowEventListeners;
-    sizeof (std::multimap<std::string,std::string>);
-    sizeof (std::pair< std::wstring, std::wstring > );
-    sizeof (std::pair<bool, float> );
-    sizeof (std::pair<bool, float> );
-    sizeof (std::pair<unsigned int,unsigned int> );
-    sizeof (std::pair<unsigned, unsigned> );
-    
-    sizeof (std::set<Entity*>);
-    sizeof (std::vector<LinkedSkeletonAnimationSource>::const_iterator );
-    sizeof (std::vector<RenderWindow*>);
-    sizeof (std::vector<ShadowRenderable*>::iterator );
-    sizeof (std::vector<ShadowTextureConfig>::const_iterator );
-    sizeof (std::vector<Vector4>);
-    sizeof (std::vector<std::string>);
-    sizeof (std::vector<String> );
-    sizeof (stdext::hash_map<std::string, unsigned short>);
-    sizeof (TRect<long int>);
-    sizeof (VectorIterator<std::vector<Bone*> >);
-    sizeof (VectorIterator<std::vector<CompositionTargetPass*> >);
-    sizeof (VectorIterator<std::vector<CompositorInstance*> >);
-    sizeof (VectorIterator<std::vector<IlluminationPass*> >);
-    sizeof (VectorIterator<std::vector<Pass*> >);
-    sizeof (VectorIterator<std::vector<RenderQueueInvocation*> >);
-    sizeof (VectorIterator<std::vector<Technique*> >);
-    sizeof (VectorIterator<std::vector<TextureUnitState*> >);
-    sizeof (VectorIterator<std::vector<VertexPoseKeyFrame::PoseRef> >);
-    
-    sizeof ( HardwareVertexBufferSharedPtr  );
-    sizeof ( StringVectorPtr );
-    sizeof ( FileInfoListPtr );
-    sizeof ( GpuProgramParametersSharedPtr );
-    sizeof ( DataStreamPtr );
-    
-    sizeof ( SharedPtr<GpuProgramParameters> );
-    sizeof ( SkeletonPtr );
-    sizeof ( SharedPtr<std::vector<std::string, std::allocator<std::string> > > );
-    sizeof ( SharedPtr<ControllerFunction<float> > );
-    sizeof ( HardwareIndexBufferSharedPtr );
-    sizeof ( GpuProgramPtr );
-    sizeof ( HighLevelGpuProgramPtr );
-    sizeof ( MeshPtr );
-    sizeof ( MaterialPtr );
-    
-    
-     
-    sizeof (std::pair<std::list<std::pair<const std::string, Ogre::MovableObject*> >::iterator,std::list<std::pair<const std::string, Ogre::MovableObject*> >::iterator>);
-    sizeof (std::pair<std::list<std::pair<const std::string, Ogre::MovableObject*> >::const_iterator,std::list<std::pair<const std::string, Ogre::MovableObject*> >::const_iterator>);
-    
-    sizeof (std::pair<const std::string,Ogre::MovableObject*>);
-    sizeof (std::pair<std::list<std::pair<const std::string, Ogre::MovableObject*> >::iterator,bool>);
-
-}
-
-} } //python_ogre::details
 
 
 #endif//__python_ogre_h_5B402FBF_8DE8_49C9_AF71_1CF9634B5344__
