b905ff44964ba77a9ef7581d94abe28da92d6d9e
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-01-13 14:47:56 +0000

    Reasonably large changes to environment.py to hopefully make things easier for Linux users and to setup for an 'automated' scons build system Updates to change Ogre/OgreRefapp version from 'CVS' to '1.4'
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@140 503f2841-2733-4260-b5db-97e3c2726e36

--- environment.py
+++ environment.py
@@ -2,54 +2,201 @@ import os
 import sys
 import getpass
 
-root_dir = None #python-ogre root directory
-gccxml_bin = None #path to gccxml binary
-python_include_dirs = None #path to python include files directory
-pyplusplus_install_dir = None #path to pygccxml & Py++ packages
 
-generated = None #path to generated files dir
-shared_ptr_dir = None #path to shared_ptr directory
+##
+##  Jan 2007 modification
+##
+##  This will now look for external config files.
+## 
+## first it will look for PythonOgreConfig_<username>.py
+##
+## if that fails it will look for PythonOgreConfig_<platform>.py
+##
+## otherwise it will use the values you enter below...
+##
+
+_LOGGING_ON = True
+
+def log ( instring ):
+    if _LOGGING_ON:
+        print instring
+
 PythonOgreMajorVersion = "0"
 PythonOgreMinorVersion = "7" # change to 0.7 due to lowercase properties
-PythonOgrePatchVersion = "0"
+PythonOgrePatchVersion = "2"
+
+##
+## these should be fine with auto create - however override them as necessary
+##
+PATH_Python = os.path.dirname( sys.executable )
+python_include_dirs = os.path.join ( PATH_Python, 'include')
+Python_lib_dirs = os.path.join ( PATH_Python, 'libs' )
+root_dir = os.path.abspath(os.path.dirname(__file__) )## The root directory is where this module is located
+
+sys.path.append( os.path.join( root_dir, 'common_utils' ) )
+shared_ptr_dir = os.path.join( root_dir, 'shared_ptr' )
+generated_dir = os.path.join( root_dir, 'generated' )
+declarations_cache_dir = os.path.join( root_dir, 'code_generators', 'cache' )
+
+_UserName = getpass.getuser()
+_ConfigSet = False
+_SystemType = os.name ## nt or posix
+_PlatformType = sys.platform ## win32, ??
+
+##
+## Now we load the user specific stuff
+##
+
+log ( "Using PATH_Python: %s\nUserName: %s\nSystemType: %s\nRoot_dir: %s" % (PATH_Python, _UserName, _SystemType, root_dir) )
+try:
+    s = 'PythonOgreConfig_' + _UserName + '.py'
+    log ( "  Trying to load Config based upon UserName: %s " % (s) )
+    execfile( os.path.join(root_dir, s ) )
+    _ConfigSet = True
+    log ( "  Success with UserName load")
+except:
+    try:
+        s= 'PythonOgreConfig_' + _SystemType + '.py'
+        log ( "  Trying to load Config based upon SystemType: %s" % (s))
+        execfile( os.path.join(root_dir, s ) )
+        _ConfigSet = True
+        log ( "  Success with SystemType load")
+    except:
+        pass   
+
+if not _ConfigSet:
+    log ( "  Going to use internal config setting")
+    ## These need to be adjusted based upon your system
+    ##
+    ## PLEASE use an external PythonOgreConfig_<username>.py to hold these value !!!!
+    ## 
+    print "\n\n You REALLY need to create a PythonOgreConfig_%s.py file with config details", _UserName
+    PATH_Boost = r'c:/development/boost'
+    PATH_LIB_Boost = r'c:/development/boost/boost/bin.v2/libs/python/build/msvc-7.1/release/threading-multi'
+    LIB_Boost = 'boost_python-vc71-mt-1_35'
+    PATH_Newton = r'c:/development/newtonsdk/sdk'
+    PATH_Ogre = r'c:/development/Ocvs/Ogrenew'
+    PATH_OgreAddons = r'c:/development/Ocvs/OgreAddons'
+    PATH_CEGUI = r'c:/development/cegui-0.5.0'
+    PATH_OIS = r'c:/development/ois'
+    if "roman" in getpass.getuser():
+        gccxml_bin = r'd:/gccxml_cvs/gccxml-build/bin/release/gccxml.exe'
+    else:
+        gccxml_bin = r'c:/development/gccxml/bin/release/gccxml.exe'
 
 
+        
+######################
+
+##  Hopefully we can make the rest of this 'automagic'  ##
+
+######################
+                    
 class ogre:
-    root_dir = None
-    include_dirs = None
-    libs_dir = None
-    dlls_dir = None
-    generated_dir = None
-    version = "CVS"
-    #version = "1.2.3"
-    cache_file = None
-    libs_files = ["ogremain", "ogreguirenderer", "ceguibase", "ode", "newton"]
-
+    active = True
+    version = "1.4"   # "1.2"
+    libs=[LIB_Boost, 'OgreMain', 'ode', 'OgreGUIRenderer', 'CEGUIBase']
+    lib_dirs = [ PATH_LIB_Boost
+                , os.path.join( PATH_Ogre, 'Samples/Common/CEGUIRenderer/lib')
+                , os.path.join( PATH_Ogre, 'OgreMain/lib/Release' )
+                , os.path.join( PATH_Ogre, 'Dependencies/lib/Release')
+                ]
+    include_dirs = [ PATH_Boost 
+                , os.path.join(PATH_Ogre,'OgreMain/include') 
+                ]
+    CCFLAGS =  ' -D"BOOST_PYTHON_MAX_ARITY=19"'
+    LINKFLAGS = ''
+     
 class ois:
+    active = True
     version= "1.0"
-    
-class ogreode:
-    version= "1.0"
-    
-
-class CEGUI:
-    version = "0.5.0b" 
-class ode:
-    version= "0.7"
-    
-class newton:
-    version= "1.0"
-    
-class FMOD:
-    version= "1.0"
+    libs=['OIS']
+    include_dirs = [ PATH_Boost 
+            , os.path.join(PATH_OIS,'includes')    ## Note the plural include's
+            ]
+    if os.name =='nt':
+        _lpath='dll'
+    else:
+        _lpath='lib'
+    lib_dirs = [ PATH_LIB_Boost 
+            , os.path.join(PATH_OIS, _lpath)
+            ]
     
 class ogrerefapp:
-    version = "1.2.3"
-
+    active = True
+    version = "1.4"
+    libs=[LIB_Boost, 'OgreMain', 'ode', 'OgreGUIRenderer', 'CEGUIBase', 'ReferenceAppLayer']
+    lib_dirs = [ PATH_LIB_Boost
+                , os.path.join( PATH_Ogre, 'Samples/Common/CEGUIRenderer/lib')
+                , os.path.join( PATH_Ogre, 'OgreMain/lib/Release' )
+                , os.path.join( PATH_Ogre, 'Dependencies/lib/Release')
+                , os.path.join( PATH_Ogre, 'ReferenceApplication/ReferenceAppLayer/lib/Release')
+                ]
+    include_dirs = [ PATH_Boost 
+                    , os.path.join(PATH_Ogre,'OgreMain/include') 
+                    , os.path.join(PATH_Ogre,'ReferenceApplication/ReferenceAppLayer/include') 
+                    , os.path.join( PATH_Ogre, 'Dependencies/include')
+                    ]
+    CCFLAGS =  ' -D"BOOST_PYTHON_MAX_ARITY=19"'
+    
 class ogrenewt:
+    active=True
     version = "1.0"
-        
-       
+    libs = ['newton', LIB_Boost, 'OgreNewt_Main']
+    if os.name =='nt':
+        _lpath='dll'
+    else:
+        _lpath='lib-mt'  
+    include_dirs = [PATH_Boost
+                    , PATH_Newton   # only one path for Newton
+                , os.path.join(PATH_Ogre,'OgreMain/include') 
+                    ]
+    lib_dirs = [ PATH_LIB_Boost
+                , os.path.join(PATH_Newton ,_lpath)
+                ,os.path.join(PATH_OgreAddons, r'ogrenewt/OgreNewt_Main/lib/Release') 
+                , os.path.join( PATH_Ogre, 'OgreMain/lib/Release' )
+                ]
+    CCFLAGS =  ' -D"BOOST_PYTHON_MAX_ARITY=19"'
+                    
+class CEGUI:
+    active = True
+    version = "0.5.0b" 
+    libs=[LIB_Boost, 'CEGUIBase.lib', 'OgreMain', 'OgreGUIRenderer' ]
+    include_dirs = [PATH_Boost
+                    ,os.path.join(PATH_CEGUI, r'include')
+                    ,PATH_CEGUI
+                    ,os.path.join ( PATH_Ogre, r'Samples/Common/CEGUIRenderer/include' )
+                    ]
+                  
+    lib_dirs = [ PATH_LIB_Boost
+                , os.path.join ( PATH_Ogre, r'Samples/Common/CEGUIRenderer/lib' )
+                , os.path.join ( PATH_Ogre, r'OgreMain/lib/Release' )
+                , os.path.join ( PATH_Ogre, r'Dependencies/lib/Release' )
+                ]
+    CCFLAGS =  ' -D"BOOST_PYTHON_MAX_ARITY=19"'
+
+                    
+#############  these are under construction and DO NOT WORK (Yet) #####################  
+  
+class ode:
+    version= "0.7"
+    include_dirs = [r'c:/development/ode/include']
+class newton:
+    version= "1.0"
+    include_dirs = [r'c:/development/newtonsdk/sdk']
+class ogreode:
+    version= "1.0"
+    include_dirs = [r'c:/development/ocvs/ogreaddons/ogreode/include',
+                    r'c:/development/ocvs/ogreaddons/ogreode/prefab/include',
+                    r'c:\development\ocvs\ogrenew\ogremain\include',
+                    r'c:/development/ode/include']
+class FMOD:
+    version= "1.0"
+    include_dirs=[r'c:\development\fmod\api\inc']
+############################################################################################
+
+## Here is the master list....
+    
 projects = {
     'ois' : ois
     , 'ogre' : ogre
@@ -60,84 +207,9 @@ projects = {
     , 'ogrenewt' : ogrenewt
     , 'FMOD' : FMOD
     , 'ogreode' : ogreode
-}
+}        
 
-if 'roman' in getpass.getuser():
-    root_dir = r'D:\dev\python-ogre\bindings-dev'
-    gccxml_bin = r'D:\dev\gccxml/gccxml.exe'
-    python_include_dirs = 'e:/python24/include'
-    pyplusplus_install_dir = r'D:\dev\language-binding\sources'
-
-    ogre.include_dirs = [r'D:\dev\python-ogre\sources\ogre_cvs\ogrenew\OgreMain\include']
-    ois.include_dirs = r'D:\OisSDK\includes'
-else:
-    root_dir = r'C:\development\pywork'
-    gccxml_bin = r'c:/development/gccxml/bin/release/gccxml.exe'
-#    python_include_dirs = 'c:/python25/include'
-#    python_include_dirs = 'c:/python24/include'
-    pyplusplus_install_dir = r'c:\development\pyplus'
-
-    if ogre.version == "CVS":
-        ogre.include_dirs  = [r'c:\development\ocvs\ogrenew\ogremain\include']
-        ogreode.include_dirs = [r'c:/development/ocvs/ogreaddons/ogreode/include',
-                            r'c:/development/ocvs/ogreaddons/ogreode/prefab/include',
-                            r'c:\development\ocvs\ogrenew\ogremain\include',
-                            r'c:/development/ode/include']
-        ogrerefapp.include_dirs = [r'c:\development\ocvs\ogrenew\ogremain\include',
-                           r'c:\development\ocvs\ogrenew\referenceapplication\referenceapplayer\include',
-                            r'c:\development\ocvs\ogrenew\dependencies\include']
-        ogrerefapp.version="CVS"
-        
-#         ogre.libs_dirs = [r'c:/development/ocvs/ogrenew/ogremain/lib/release', 
-#                         r'c:/development/ocvs/ogrenew/samples/common/ceguirenderer/lib',
-#                         r'c:/development/ocvs/ogrenew/dependencies/lib/release',
-#                         r'c:/development/boost/boost' ]
-        CEGUI.include_dirs = [r'c:/development/cegui-0.5.0/include',
-                           ## r'c:/development/ocvs/ogrenew/dependencies/include',
-                           ## NOTE: ogre headers expect cegui to live in ./CEGUI so I've copied the cegui include
-                           ## tree to ./CEGUI in the following directory..
-                            r'c:/development/ocvs/ogrenew/dependencies/include/',
-                            r'c:/development/ocvs/ogrenew/samples/common/CEGUIRenderer/include',
-                            r'c:/development/ocvs/ogrenew/ogremain/include',
-                            r'c:/development/boost'
-                            ]
-                            
-        ogrenewt.include_dirs = [r'c:/development/ocvs/ogreaddons/ogrenewt/OgreNewt_Main/inc',
-                            r'c:/development/ocvs/ogrenew/ogremain/include',
-                            r'c:/development/newtonsdk/sdk',
-                            r'c:/development/boost',
-                            r'c:/python25/include'
-                            ]
-
-    else:
-        ogre.include_dirs  = [r'c:\devold\ogreSDK\include',
-                                r'c:\devold\ogreSDK\samples\refapp\include'
-                                ]
-        CEGUI.include_dirs = [r'c:/devold/cegui-0.5.0/include',
-                            r'c:/devold/ogresdk/samples/include',
-                            r'c:/devold/ogresdk/include',
-                            r'c:/development/boost'
-                            ]  
-                                    
-        ogrerefapp.include_dirs = ogre.include_dir
-        
-#    CEGUI.include_dir = r'c:/ogresdk/include/CEGUI'
-    ode.include_dirs = [r'c:/development/ode/include']
-    newton.include_dirs = [r'c:/development/newtonsdk/sdk']
-    ois.include_dirs = [r'c:\development\ois\includes']
-    FMOD.include_dirs=[r'c:\development\fmod\api\inc']
     
-
-
-sys.path.append( os.path.join( root_dir, 'common_utils' ) )
-sys.path.append( pyplusplus_install_dir + r'/pygccxml_dev' )
-sys.path.append( pyplusplus_install_dir + r'/pyplusplus_dev' )
-
-shared_ptr_dir = os.path.join( root_dir, 'shared_ptr' )
-generated_dir = os.path.join( root_dir, 'generated' )
-#generated_dir = 'c:/development/boost/libs/pythonogre'
-
-declarations_cache_dir = os.path.join( root_dir, 'code_generators', 'cache' )
 
 for name, cls in projects.items():
     cls.root_dir = os.path.join( root_dir, 'code_generators', name )
