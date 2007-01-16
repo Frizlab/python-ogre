b577946405c195fba93f76630bd191f9cba07888
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-01-16 16:25:38 +0000

    changed tostring from a class to a "member_functions" - bug fix
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@149 503f2841-2733-4260-b5db-97e3c2726e36

--- code_generators/ogre/generate_code.py
+++ code_generators/ogre/generate_code.py
@@ -182,7 +182,7 @@ def filter_declarations( mb ):
     
     #as reported by mike with linux:bp::arg("flags")=(std::_Ios_Fmtflags)0
     if os.name == 'posix':
-        ogre_ne.class_('StringConverter').class_('toString').exclude()    
+        ogre_ne.class_('StringConverter').member_functions('toString').exclude()    
         
     
         
