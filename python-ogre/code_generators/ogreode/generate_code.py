4e40246b0280b4ba1935b1401b28615cb1015040
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-01-25 13:05:36 +0000

    Various small updates
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@160 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/code_generators/ogreode/generate_code.py
+++ python-ogre/code_generators/ogreode/generate_code.py
@@ -35,7 +35,7 @@ def filter_declarations( mb ):
     ogreode_ns.include()
     ogreode_ns.class_("Body").variable("MovableType").exclude() ## this "static const Ogre::String" causes msvc7.1 to die!!
     ogreode_ns.class_("Utility").variable("Infinity").exclude() ## this "static const Ogre::String" causes msvc7.1 to die!!
-    ogreode_ns.class_("Body").variable("getTypeName").exclude() ## this "static const Ogre::String" causes msvc7.1 to die!!
+#    ogreode_ns.class_("Body").variable("getTypeName").exclude() ## this "static const Ogre::String" causes msvc7.1 to die!!
 
     ogreode_ns.class_("EntityInformer").mem_fun("getIndices").exclude() ## unsigned int const *
     
