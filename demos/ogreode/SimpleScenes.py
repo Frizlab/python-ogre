bdd36b253ae900b88288e0a1d244a6706771c67e
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-02-06 16:19:14 +0000

    Update to generate_code.py to fix UTFString creation and highlight typedef exclusion problem First round of demo for Ogreode -- doesn't work yet but thought I'd put it up in case others want to work on it..
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@174 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ demos/ogreode/SimpleScenes.py
@@ -0,0 +1,308 @@
+# /*
+# SimpleScenes.cpp
+# ---------------
+# Code for the base scene class from which the specific scenes are derived
+# */
+import Ogre as ogre
+import OgreOde
+# 
+# base class 
+# 
+class SimpleScenes (OgreOde.CollisionListener,OgreOde.StepListener):
+    KEY_DELAY = 1.0
+    STEP_RATE = 0.01
+    def __init__ ( self, world ):
+        OgreOde.CollisionListener.__init__(self)
+        OgreOde.StepListener.__init__(self)
+     
+        self._world = world
+        self._mgr = _world.getSceneManager()
+        self._world.setCollisionListener(self)
+        self._space = _world.getDefaultSpace()
+        self._key_delay = KEY_DELAY
+        self._last_node = 0
+        self.RagdollList=[]
+        
+        self.dotOgreOdeLoader = OgreOde.DotLoader( world )
+        self.bodies=[]  # an array to keep objects around in (like c++ "new" )
+        self._ragdollFactory = OgreOde_Prefab.RagdollFactory()
+        ogre.Root.getSingletonPtr().addMovableObjectFactory(self._ragdollFactory) 
+        setInfoText("")
+        
+        self.xmlNames = [
+            "zombie",
+            "zombie",
+            "ninja",
+            "robot"
+            ]
+            
+        self.meshNames = [
+            "zombie_small.mesh",
+            "zombie_small.mesh",
+            "ninja.mesh",
+            "robot.mesh"
+            ]
+        self.meshAnimation = [
+            "Walk1",
+            "Walk1",
+            "Walk",
+            "Walk"
+            ]
+        self.meshScale = [
+            ogre.Vector3(1.0, 1.0, 1.0),
+            ogre.Vector3(1.0, 1.0, 1.0),
+            ogre.Vector3(1.0, 1.0, 1.0),
+            ogre.Vector3(1.0, 1.0, 1.0)
+            ]
+        
+        self.ragdollFile = [
+            "zombie.ogreode",
+            "zombie_auto.ogreode",
+            "ninja.ogreode",
+            "robot.ogreode"
+            ]
+        self.sSelectedMesh = 0
+        self._ragdoll_count = 0        
+        
+    
+    # 
+    # Called by OgreOde whenever a collision occurs, so 
+    # that we can modify the contact parameters
+    # 
+    def collision( self, contact) :
+        ## Check for collisions between things that are connected and ignore them
+        g1 = contact.getFirstGeometry()
+        g2 = contact.getSecondGeometry()
+    
+        if (g1 and g2):
+            b1 = g1.getBody()
+            b2 = g2.getBody()
+            if (b1 and b2 and OgreOde.Joint.areConnected(b1, b2)):
+               return False 
+    
+        ## Set the friction at the contact
+        contact.setCoulombFriction(OgreOde.Utility.Infinity)
+        contact.setBouncyness(0.1)
+    
+        ## Yes, this collision is valid
+        return True
+
+# 
+# Handle key presses
+# 
+    def frameEnded(self, time,keyinput, mouse):
+        self._key_delay += time
+        for d in self.RagdollList:
+            d.update()  # ??
+
+    # 
+    # Utility method to set the information string in the UI
+    # 
+    def setInfoText(self,  text):
+        ogre.OverlayManager.getSingleton().getOverlayElement("OgreOdeDemos/Info").setCaption(ogre.UFTString("Info: " + text))
+
+        
+
+# /*
+# Create a ragdoll
+# */
+    def createRagDoll( self ):
+        global KEY_DELAY
+        if (self._key_delay < KEY_DELAY): 
+            return
+     
+        self._key_delay = 0.0
+    
+        params = ogre.NameValuePairList()
+    
+        params["mesh"] = self.meshNames[self.sSelectedMesh]
+    
+        RagDollName = "zombie" + str(self.self._ragdoll_count)
+        self.self._ragdoll_count += 1
+        _ragdoll = self._mgr.createMovableObject(RagDollName, 
+            OgreOde_Prefab.RagdollFactory.FACTORY_TYPE_NAME,
+            params)
+            
+        _ragdoll.setCastShadows(True)
+    
+        _ragdoll_node = self._mgr.getRootSceneNode().createChildSceneNode(_ragdoll.getName() + "Node")
+        _ragdoll_node.attachObject(_ragdoll)
+    
+        ##_ragdoll_node.yaw(Degree(rand() % 360))
+        ##_ragdoll_node.pitch(Degree(rand() % 360))
+    
+    
+        _ragdoll_node.setOrientation(Quaternion(ogre.Radian(OgreOde.Utility.randomReal() * 10.0 - 5.0),
+                                        ogre.Vector3(OgreOde.Utility.randomReal() * 2.0 - 1.0,
+                                        OgreOde.Utility.randomReal() * 2.0 - 1.0,
+                                        OgreOde.Utility.randomReal() * 2.0 - 1.0)))
+    
+        _ragdoll_node.setPosition(ogre.Vector3((OgreOde.Utility.randomReal() * 10.0) - 5.0,
+                                    OgreOde.Utility.randomReal() + 5,
+                                    (OgreOde.Utility.randomReal() * 10.0) - 5.0))
+    
+    
+        _ragdoll_node.setScale( self.meshScale[self.sSelectedMesh])
+    
+    
+        _ragdoll.getAnimationState(self.meshAnimation[self.sSelectedMesh]).setEnabled(False)
+    
+        if (self.ragdollFile[self.sSelectedMesh] != "" ):
+            dotOgreOdeLoader.loadRagdoll(self.ragdollFile[self.sSelectedMesh], 
+                                        _ragdoll,
+                                        self.xmlNames[self.sSelectedMesh])
+    
+        ## Create the ragdoll
+        _ragdoll.takePhysicalControl(_space, False)
+        _ragdoll.setSelfCollisions(False)
+    
+        self.RagdollList.append(_ragdoll)
+
+    # /*
+    # Create a randomly sized box, sphere or capsule for dropping on things
+    # */ 
+    def createRandomObject(self, objectClass):
+        if (self._key_delay < self.KEY_DELAY): 
+            return 0
+    
+        if objectClass == OgreOde.Geometry.Class_Box:
+            typeName = "crate"
+        elif objectClass == OgreOde.Geometry.Class_Sphere:
+            typeName = "ball" 
+        elif objectClass == OgreOde.Geometry.Class_Capsule:
+            typeName = "capsule" 
+        elif objectClass == OgreOde.Geometry.Class_TriangleMesh:
+            typeName = "gun" 
+        else:
+            typeName = "unknown" 
+            
+        ## Create the visual representation (the Ogre entity and scene node)
+        name = typeName + str(_bodies.size())
+        entity = self._mgr.createEntity(name, typeName + ".mesh")
+        node = self._mgr.getRootSceneNode().createChildSceneNode(name)
+        node.attachObject(entity)
+        entity.setNormaliseNormals(True)
+        entity.setCastShadows(True)
+    
+        ## Pick a size
+        size = ogre.Vector3((OgreOde.Utility.randomReal() * 0.5 + 0.1) * 2.0,
+                    (OgreOde.Utility.randomReal() * 0.5 + 0.1) * 2.0,
+                    (OgreOde.Utility.randomReal() * 0.5 + 0.1) * 2.0)
+    
+        ## Create a body associated with the node we created
+        body = OgreOde.Body()
+        node.attachObject(body)
+    
+        ## Set the mass and geometry to match the visual representation
+        if objectClass == OgreOde.Geometry.Class_Box:
+            size *= 1.75
+    
+            mass = OgreOde.BoxMass(1.0,size)
+            mass.setDensity(5.0,size)
+            
+            geom = OgreOde.BoxGeometry(size,_space)
+            node.setScale(size.x * 0.1,size.y * 0.1,size.z * 0.1)
+            body.setMass(mass)
+    
+        elif objectClass == OgreOde.Geometry.Class_Sphere:
+            mass= OgreOde.SphereMass(1.0,size.x)
+            mass.setDensity(5.0,size.x)
+    
+            geom = OgreOde.SphereGeometry(size.x,_space)
+            node.setScale(size.x * 0.2,size.x * 0.2,size.x * 0.2)
+            body.setMass(mass)
+    
+        elif objectClass == OgreOde.Geometry.Class_Capsule:
+                size.x *= 0.5
+    
+                mass = OgreOde.CapsuleMass(1.0,size.x,Vector3.UNIT_Z,size.y)
+                mass.setDensity(5.0,size.x,Vector3.UNIT_Z,size.y)
+                
+                geom = OgreOde.CapsuleGeometry(size.x,size.y,_space)
+                node.setScale(size.x,size.x,(size.y + (size.x * 2.0)) * 0.25)
+                body.setMass(mass)
+    
+        elif objectClass == OgreOde.Geometry.Class_TriangleMesh:
+                ei = OgreOde.EntityInformer (entity, node._getFullTransform())
+                geom = ei.createStaticTriangleMesh(_space)
+    
+                mass = OgreOde.BoxMass (1.0, ei.getSize())
+                mass.setDensity(5.0, ei.getSize())
+    
+                ##node.setScale(size.x,size.x,(size.y + (size.x * 2.0)) * 0.25)
+                body.setMass(mass)
+    
+        ## Tie the collision geometry to the physical body
+        geom.setBody(body)
+    
+        ## Keep track of the body
+        self._bodies.append(body)
+        self._geoms.append(geom)
+        
+        self._key_delay = 0.0
+    
+        ## If we created something position and orient it randomly
+        if (body) :
+            body.setOrientation(ogre.Quaternion(ogre.Radian(OgreOde.Utility.randomReal() * 10.0 - 5.0),
+                                    Vector3(OgreOde.Utility.randomReal() * 2.0 - 1.0,
+                                            OgreOde.Utility.randomReal() * 2.0 - 1.0,
+                                            OgreOde.Utility.randomReal() * 2.0 - 1.0)))
+            body.setPosition(ogre.Vector3((OgreOde.Utility.randomReal() * 10.0) - 5.0,
+                                        OgreOde.Utility.randomReal() + 5,
+                                        (OgreOde.Utility.randomReal() * 10.0) - 5.0))
+    
+            ## Set the last body we created to be looked at
+            _last_node = body.getParentNode()
+    
+        return body
+    
+    # 
+    # Destructor, delete everything we're keeping track of
+    # 
+    def __del__ ( self ):
+        ## Stop listening for collisions
+        if (self._world.getCollisionListener() == self): 
+            _world.setCollisionListener(0)
+    
+        ## Delete all the joints
+        for i in self._joints:
+            del (i)
+        clearList = []
+        ## Run through the list of bodies we're monitoring
+        for i in self._bodies:
+            ## Get the node this body controls
+            node = i.getParentNode()
+            if (node):
+                ## Get its name and remember all the things attached to it
+                name = node.getName()
+                num = node.numAttachedObjects()
+                for cur in range ( num ) :
+                    obj = node.getAttachedObject(cur)
+                    if (obj.getMovableType() != OgreOde.Body.MovableType): 
+                        clearList.append(obj)
+    
+                ## Destroy the node by name
+                self._mgr.getRootSceneNode().removeAndDestroyChild(name)
+            ## Delete the body
+            del (i)
+    
+        ## Remove all the entities we found attached to scene nodes we're controlling
+        for i in clearList:
+            if (i.getMovableType() == "Entity") :
+                self._mgr.destroyMovableObject(i)
+            elif (i.getMovableType() == "ParticleSystem")  :
+                _mgr.destroyParticleSystem(i)
+    
+        ## Delete all the collision geometries
+        for i in self._geoms:
+            del (i)
+        ## Remove all the entities we found attached to scene nodes we're controlling
+        for i in self.RagdollList:
+            assert (i.getParentNode ())
+            assert (i.getParentNode ().getParent())
+            i.getParentNode().getParent().removeAndDestroyChild(
+                i.getParentNode().getName ())
+    
+            self._mgr.destroyMovableObject(i.getName(), OgreOde_Prefab.RagdollFactory.FACTORY_TYPE_NAME)
+        ogre.Root.getSingletonPtr().removeMovableObjectFactory(_ragdollFactory)
+        del _ragdollFactory 
