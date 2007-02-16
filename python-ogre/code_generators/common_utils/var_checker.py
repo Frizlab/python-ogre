a7348a806f12edcefd53c94923c2218809a3a21e
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-02-16 01:09:30 +0000

    Missing file
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@186 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python-ogre/code_generators/common_utils/var_checker.py
@@ -0,0 +1,40 @@
+"""
+Check of the variable is being 'set' in the header file
+This is BAD for static const vars as the compile fails in Linux
+"""
+
+class var_checker:
+    """
+    Checks for '=' in var defination line
+    """
+    def __init__(self):
+        #for caching source
+        self.file_name  = None
+        self.source = None
+        
+    def __call__(self, declaration):
+        #print "var_checker called with:", declaration
+        try:    
+            if self.file_name != declaration.location.file_name:            
+                self.file_name = declaration.location.file_name
+                self.source = open(declaration.location.file_name).readlines()
+        except:
+            print "Var Checker called without location:", declaration
+            return ""
+            
+        ## note the gccxml uses a 1 based line scheme whereas we are using python arrays - 0 based
+        ## so we need to subtract 1 from the line number.
+        
+        varline = self.source[declaration.location.line-1]
+#         print "Checker info:",declaration.location.line, declaration.location.file_name
+#         print "Checker Line:", varline
+        if not '=' in varline:
+            return False
+        # do a simple check to ensure the '=' hasn't been commented out
+        for char in varline:
+            if char =='/' or char =='\\':
+                return False
+            elif char == '=':
+                return True
+        return False
+        
