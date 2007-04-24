824b81a3d5082dc8edf0f0aea1956776a90e60e3
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-04-24 01:18:58 +0000

    Shared Pointer changes for GCC4.1 ??
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@268 503f2841-2733-4260-b5db-97e3c2726e36

--- shared_ptr/py_shared_ptr.h
+++ shared_ptr/py_shared_ptr.h
@@ -4,16 +4,14 @@
 #include "boost/python.hpp"
 #include "Ogre.h"
 
+namespace Ogre {
 template<class T>
 inline T * get_pointer(Ogre::SharedPtr<T> const& p){
     return p.get();
 }
-
+}
 
 namespace boost{ namespace python{
-
-    using ::get_pointer;
-    
     template <class T>
     struct pointee< Ogre::SharedPtr<T> >{
         typedef T type;
