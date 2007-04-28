c1c5565592dd8d447689678da3d33312999414dd
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-04-28 02:12:51 +0000

    Rename code generators dir from CEGUI to cegui (for consistancy)
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@277 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python-ogre/code_generators/cegui/customization_data.py
@@ -0,0 +1,15 @@
+        
+def header_files( Version ):
+    return [   "CEGUI.h"
+            , "CEGUIXMLParser.h"
+            , "OgreCEGUIRenderer.h"
+            , "CEGUIXMLHandler.h"
+            , "CEGUIXMLAttributes.h"
+            , "CEGUIXMLSerializer.h"
+            , "OgreCEGUIRenderer.h"
+            ,"OgreCEGUIResourceProvider.h"
+                  ]
+
+
+def huge_classes( Version ):
+    return []
