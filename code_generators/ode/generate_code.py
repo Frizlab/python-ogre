ae1c4751af5c2e871452a6cbc4c337bec8fdb663
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-05-29 07:12:42 +0000

    Fix as I'd reverted to an older version
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@335 503f2841-2733-4260-b5db-97e3c2726e36

--- code_generators/ode/generate_code.py
+++ code_generators/ode/generate_code.py
@@ -128,9 +128,12 @@ def filter_declarations( mb ):
     ode_ns = global_ns  ##  Ode doesn't have it's own namespace..  .namespace( 'ode' )
     for cls in ode_ns.classes():
         print "Checking ", cls.decl_string
-        if  cls.decl_string[2]=='d' and cls.decl_string[3].isupper():
-            print "Including Class:", cls.name
-            cls.include()
+        try:
+            if  cls.decl_string[2]=='d' and cls.decl_string[3].isupper():
+                print "Including Class:", cls.name
+                cls.include()
+        except:
+            pass
     ## and the dxXXclasses        
     for cls in ode_ns.classes():
         print "Checking ", cls.decl_string
@@ -168,7 +171,10 @@ def filter_declarations( mb ):
         "dWorldSetAutoDisableAngularAverageThreshold",
         "dWorldSetAutoDisableLinearAverageThreshold"     )
     for cls in ignore:
-        ode_ns.free_function(cls).exclude()
+        try:
+            ode_ns.free_function(cls).exclude()
+        except:
+            pass
        
     # #     
     # in hand wrappers to handle pyobjects...
