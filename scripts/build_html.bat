1ef269240ce2ac3951668418dbf5942209a41487
amiller <amiller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-06-07 01:17:11 +0000

    Scripts to help build the html and chm documentation
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@347 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ scripts/build_html.bat
@@ -0,0 +1,21 @@
+echo off
+echo =========================================
+echo Python-Ogre batch file to generate HTML help 
+echo edit "epydoc_config" if you want to change anything
+echo NOTE: import ogre.renderer.OGRE must be added to \python25\scripts\epydoc for this to work
+echo =========================================
+\python25\python \python25\scripts\epydoc --config epydoc_config
+echo
+echo =========================================
+echo now to build the compiled help
+echo first we try with chmgenerator
+python ..\..\chmgenerator\gen_epydoc.py c:\temp\html -o c:\temp\doc -n Python-Ogre -c c:\apps\hc\hcp.exe
+echo 
+echo Now it's likely that this fails, however it's created the Python-Ogre.hpp config file so you can
+echo run the microsfot htmlhtml studio and compile the chm
+rem
+rem https://frogpie.svn.sourceforge.net/svnroot/frogpie/trunk/sandbox/chm (chm generator)
+rem http://msdn.microsoft.com/library/en-us/htmlhelp/html/hwMicrosoftHTMLHelpDownloads.asp 
+rem https://svn.sourceforge.net/svnroot/epydoc/trunk/epydoc
+rem Checkout a new gen_epydoc.py in the patch directory if you have issues
+
