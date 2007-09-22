11852bc7a600deae5a0178ce37790ffef7fd99e0
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-09-22 13:44:26 +0000

    Updates for latest bullet version
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@389 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/code_generators/bullet/hand_made_wrappers.py
+++ python-ogre/code_generators/bullet/hand_made_wrappers.py
@@ -1,8 +1,21 @@
 import os
 import environment
 
-
-
+WRAPPER_WRAPPER_getName =\
+"""
+    virtual char * getName(  ) const {
+        bp::override func_getName = this->get_override( "getName" );
+        return func_getName(  );
+    }
+"""
+WRAPPER_WRAPPER_getInternalManifoldPointer =\
+"""
+    virtual ::btPersistentManifold * * getInternalManifoldPointer(  ){
+        bp::override func_getInternalManifoldPointer = this->get_override( "getInternalManifoldPointer" );
+        return func_getInternalManifoldPointer(  );
+    }
+    """
+    
 #################################################################################################
 #################################################################################################
 
@@ -11,5 +24,11 @@ def apply_reg ( class_, code ):
         class_.add_registration_code ( c )
         
 def apply( mb ):
-    pass
-    
+
+    rt = mb.class_( 'btCollisionShape' )   
+    rt.add_wrapper_code ( WRAPPER_WRAPPER_getName )
+    rt = mb.class_( 'btConcaveShape' )   
+    rt.add_wrapper_code ( WRAPPER_WRAPPER_getName )
+    rt = mb.class_( 'btDispatcher' )   
+    rt.add_wrapper_code ( WRAPPER_WRAPPER_getInternalManifoldPointer )
+#     
