c6d3dc5db7c212b311522eab73cb67f8589ffb5d
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-10-22 03:57:27 +0000

    Allow libpath override
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@450 503f2841-2733-4260-b5db-97e3c2726e36

--- demos/PythonOgreConfig.py
+++ demos/PythonOgreConfig.py
@@ -3,18 +3,28 @@
 # this is primarly used to manage Linux demos when we have binaries in non standard (safe) locations
 
 import os,sys
+
+#########################################################################
+### Override this setting if you are using non standard locations
+
+libpath = ""
+
+#########################################################################
+
+
 if os.name == 'nt':
     libpath=""
     pythonpath = ""
 else:    
-    localpath = os.path.abspath('.')   ## should be one of the demo directories
-    libpath = os.path.abspath(os.path.join(localpath,'../../lib'))
-    if not os.path.exists ( libpath ):   #we must be running from the development environment
-        libpath = os.path.abspath(os.path.join(localpath,'../../../root/usr/lib'))
-        if not os.path.exists ( libpath ):
-            ## should die here
-            print "**Problem finding libraries"
-            libpath=localpath 
+    if libpath == "" :  # we need to find this ourselves
+        localpath = os.path.abspath(os.path.dirname(__file__) ) # assume that this module is in the ./demos directory
+        libpath = os.path.abspath(os.path.join(localpath,'../lib'))  # so in the binary release the lib is one path back up
+        if not os.path.exists ( libpath ):   #we must be running from the development environment
+            libpath = os.path.abspath(os.path.join(localpath,'../../root/usr/lib')) 
+            if not os.path.exists ( libpath ):
+                ## should die here
+                print "**Problem finding libraries"
+                libpath=localpath 
                
     ## path to the Python-Ogre modules        
     pythonpath = os.path.abspath(os.path.join(libpath, 'python2.5/site-packages') )
