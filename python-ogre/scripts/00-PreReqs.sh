4ba62dd33b7f69f3e2ef13e8a9ecf9276021748a
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-11-13 10:39:26 +0000

    Add patch to the prereqs...
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@475 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/scripts/00-PreReqs.sh
+++ python-ogre/scripts/00-PreReqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Part of the Python-Ogre installation 
 sudo apt-get install g++ libfreetype6-dev libpcre3-dev autoconf automake1.9 libgl1-mesa-dev libx11-dev \
-libglut3-dev libtool libxaw7-dev libxxf86vm-dev \
+libglut3-dev libtool libxaw7-dev libxxf86vm-dev patch \
 libxrandr-dev cvs subversion libxt-dev libexpat1-dev python python-dev \
 python-celementtree libglew-dev 
