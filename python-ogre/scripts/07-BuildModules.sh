b6662e1d5ef520f32e3c2e5ce68f0a0e6281d2f6
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2007-12-04 13:23:43 +0000

    Change build to call scons seperately for each module as there is a bug in scons where it uses the command line as it's detection for a rebuild which was causing excess building
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@489 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/scripts/07-BuildModules.sh
+++ python-ogre/scripts/07-BuildModules.sh
@@ -12,7 +12,8 @@ if [ $1 != "" ]
 then 
     scons PROJECTS=$1 
 else
-    scons PROJECTS=ogre,ois,caelum,cegui,plib,ogrenewt,ogreode,quickgui,et,bullet,ogreforests -i
+    for PROJ in ogre ois cegui quickgui; do scons PROJECTS=$PROJ ; done ;
+    for PROJ in plib ogrenewt ogreode caelum et ogreforests bullet ogrebulletc ogrebulletd ; do scons PROJECTS=$PROJ ; done ;
 #ogreal,ogrebulletc,ogrebulletd,nxogre -i
 fi
 #opcode -- not really suited to a linux build -- lots of work needed ? 
