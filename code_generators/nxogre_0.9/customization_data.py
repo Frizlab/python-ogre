9ac3a685674fd1d281cd2749673a027644e7c65f
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2008-01-16 05:25:37 +0000

    NxOgre 0.9 work
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@548 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ code_generators/nxogre_0.9/customization_data.py
@@ -0,0 +1,13 @@
+
+def header_files( version ):
+    return [ 'NxOgre.h'
+            ,'NxOgreSkeleton.h'
+            ,'NxOgreResourceMesh.h'
+            ,'Ogre.h'
+            ,'OgrePlugin.h'
+            ,'NxPhysics.h'
+            ]
+  
+def huge_classes( version ):
+    return []
+   
