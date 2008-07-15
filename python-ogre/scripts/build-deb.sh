ccc327bc805c8dae6ab14a6c5b6d024e8998cf85
mithro <mithro@503f2841-2733-4260-b5db-97e3c2726e36>
2008-07-15 13:10:54 +0000

    Fail hard on errors building the prereqs.
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@644 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/scripts/build-deb.sh
+++ python-ogre/scripts/build-deb.sh
@@ -39,24 +39,22 @@ fi
 # Things we need to build ourself
 
 # The extra boost support module
-python python-ogre/BuildModule.py -r --build-deb boost_python_index || exit
+python python-ogre/BuildModule.py --failhard -r --build-deb boost_python_index || exit
 sudo true
-python python-ogre/BuildModule.py --install-deb boost_python_index || exit
+python python-ogre/BuildModule.py --failhard --install-deb boost_python_index || exit
 # The gccxml version is important to us (but only during build)
-python python-ogre/BuildModule.py -r --build-deb gccxml || exit
+python python-ogre/BuildModule.py --failhard -r --build-deb gccxml || exit
 sudo true
-python python-ogre/BuildModule.py --install-deb gccxml || exit
+python python-ogre/BuildModule.py --failhard --install-deb gccxml || exit
 # The pygccxml is dependent on the gccxml version we just built
-python python-ogre/BuildModule.py -r --build-deb pygccxml  || exit
+python python-ogre/BuildModule.py --failhard -r --build-deb pygccxml  || exit
 sudo true
-python python-ogre/BuildModule.py --install-deb pygccxml  || exit
-
-#python python-ogre/BuildModule.py -r --build-deb boost || exit
+python python-ogre/BuildModule.py --failhard --install-deb pygccxml  || exit
 
 # Build our custom extension to boost-indexing which pyplusplus needs
-python python-ogre/BuildModule.py -r --build-deb pyplusplus || exit
+python python-ogre/BuildModule.py --failhard -r --build-deb pyplusplus || exit
 sudo true
-python python-ogre/BuildModule.py --install-deb pyplusplus || exit
+python python-ogre/BuildModule.py --failhard --install-deb pyplusplus || exit
 
 # Generate the python-ogre package
 cd python-ogre
