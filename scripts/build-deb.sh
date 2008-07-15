52d31dc565fa0af5b019288cf520e5b8850fd75d
mithro <mithro@503f2841-2733-4260-b5db-97e3c2726e36>
2008-07-15 12:35:36 +0000

    Add better error detection. Added missing dependencies. Added variations for different distros.
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@640 503f2841-2733-4260-b5db-97e3c2726e36

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,10 +1,25 @@
 #! /bin/sh
 
+if [ ! -x python-ogre ]; then
+	echo "The python-ogre directory does not exist,"
+	echo "please make sure you are running this script"
+	echo "from the directory you check out the code."
+	exit
+fi
+
 # Run sudo so that we have the permissions to do things
 sudo true
 
+. python-ogre/scripts/00-PreReqs.sh
+
 # The following modules are provided by the operating system
-sudo apt-get install \
+sudo apt-get install -y \
+	debhelper \
+	fakeroot \
+	chrpath \
+	python-all \
+	python-setuptools \
+	\
 	cmake \
 	scons \
 	nvidia-cg-toolkit \
@@ -14,7 +29,12 @@ sudo apt-get install \
 	libfreeimage-dev \
 	libois-dev \
 	libogre-dev \
-	libceguiogre-dev
+	libopenal-dev
+
+DISTRO_CODENAME=`lsb_release -c | sed -e's/.*:\t*//'`
+if [ "x$DISTRO_CODENAME" = "xhardy" ]; then
+	sudo apt-get install libceguiogre-dev
+fi
 
 # Things we need to build ourself
 
@@ -41,4 +61,5 @@ python python-ogre/BuildModule.py --install-deb pyplusplus || exit
 # Generate the python-ogre package
 cd python-ogre
 dpkg-checkbuilddeps || exit
+chmod a+x ./debian/rules
 fakeroot ./debian/rules binary
