1f75fc9db1e624d1ddeb6e8545f07ca9525bb01a
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2008-07-29 09:34:29 +0000

    Initial Implementation for the PCZ Portal Scene Manager
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@676 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python-ogre/code_generators/ogrepcz/customization_data.py
@@ -0,0 +1,17 @@
+
+def header_files( version ):
+    return ["Ogre.h",
+            "OgrePortal.h",
+            "OgrePCZSceneNode.h",
+            "OgrePCZone.h",
+            "OgrePCZSceneManager.h",
+            "OgrePCZSceneQuery.h",
+            "OgrePCZLight.h",
+            "OgrePCPlane.h",
+            "OgrePCZFrustum.h"
+
+            ]
+  
+def huge_classes( version ):
+    return []
+   
