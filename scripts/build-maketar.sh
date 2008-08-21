c5c521bc5c0b0066c106c32a0c4f105dfc6148bd
mithro <mithro@503f2841-2733-4260-b5db-97e3c2726e36>
2008-08-21 02:26:07 +0000

    Commit a script to build a tarball for the OpenSuse build service.
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@709 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ scripts/build-maketar.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+rm -rvf python-ogre
+
+cd python-ogre-pristine
+svn update
+cd ..
+cp -rvf python-ogre-pristine/ python-ogre/
+
+rm -rvf \
+	python-ogre/boost 		 						\
+	python-ogre/debs 		 						\
+	python-ogre/debian 		 						\
+	python-ogre/demos 								\
+	python-ogre/docs 								\
+	python-ogre/packages_2.6 						\
+	python-ogre/patch 								\
+	python-ogre/ogre_cvs.sh 						\
+	python-ogre/scripts 							\
+	python-ogre/SourceSnapShots 					\
+	python-ogre/ThirdParty/extra 					\
+	python-ogre/ThirdParty/dshow 					\
+	python-ogre/ThirdParty/History 					\
+	python-ogre/ThirdParty/vcredist_x86.exe 		\
+
+
+find python-ogre -name .svn | xargs rm -rf
+
+touch python-ogre/STABLE
+
+tar -czvf python-ogre.tar.gz python-ogre
+md5sum python-ogre.tar.gz
+du -k python-ogre.tar.gz
+du -h python-ogre.tar.gz
+
+cp python-ogre.tar.gz ..
+cp python-ogre-pristine/debian/rules ../debian.rules
+cp python-ogre-pristine/debian/control ../debian.control
+cp python-ogre-pristine/debian/changelog ../debian.changelog
