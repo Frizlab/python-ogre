012189352e65bd2adf178d367b18b778272dae84
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2008-08-31 01:25:05 +0000

    Adding initial wrapper for hikari to the svn
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@712 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python-ogre/code_generators/hikari/python_hikari.h
@@ -0,0 +1,19 @@
+
+#include "Hikari.h"
+
+// First we create a magic namespace to hold all our aliases
+namespace pyplusplus { namespace aliases {
+    
+ #include "python_hikari_aliases.h"
+} } 
+
+// then we exposed everything needed (and more) to ensure GCCXML makes them visible to Py++
+//
+namespace python_hikari{ namespace details{
+inline void instantiate(){
+ using namespace Hikari;
+ #include "python_hikari_sizeof.h"
+ 
+} } }
+
+
