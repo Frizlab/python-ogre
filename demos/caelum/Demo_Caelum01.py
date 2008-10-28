880a6332295f8c8f3cce0e5712415879a74a91a1
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2008-10-28 14:09:34 +0000

    Update to latest Caelum and demo working again
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@779 503f2841-2733-4260-b5db-97e3c2726e36

--- demos/caelum/Demo_Caelum01.py
+++ demos/caelum/Demo_Caelum01.py
@@ -1,6 +1,6 @@
 ## /*
 ## -----------------------------------------------------------------------------
-## This source file is based on Caelum's CaelumTest1
+## This source file is based on Caelum's CaelumDemo
 ## -----------------------------------------------------------------------------
 ## */
 import sys
@@ -13,89 +13,58 @@ import ogre.addons.caelum as caelum
 import SampleFramework as sf
 import ctypes
 
-## ------------------------------------------------------------------ 
-## our basic Python Ogre TerrainFrame Listener
-## ------------------------------------------------------------------ 
-class TerrainFrameListener(sf.FrameListener):
+class CaelumSampleFrameListener(sf.FrameListener):
 
-    def __init__(self, renderWindow, camera, sceneManager):
-        sf.FrameListener.__init__(self, renderWindow, camera)
-
-        self.sceneManager = sceneManager
-        self.camera = camera
-        self.moveSpeed = 50.0
-        self.updateRay = ogre.Ray() 
-        ## basic terrain ray query
-        pos = camera.getPosition()
-        d = ogre.Vector3().NEGATIVE_UNIT_Y
-        self.raySceneQuery = sceneManager.createRayQuery(ogre.Ray( pos, d ))
-
-    ## -------------------------------------------------------------- 
-    def frameStarted(self, frameEvent):
-
-        ## call parent framelistener
-        ret =  sf.FrameListener.frameStarted(self, frameEvent)
-
-        ## clamp camera to terrain
-        self.updateRay.setOrigin (self.camera.getPosition())
-        self.updateRay.setDirection (ogre.Vector3().NEGATIVE_UNIT_Y)
-        self.raySceneQuery.Ray = self.updateRay
-        for queryResult in self.raySceneQuery.execute():
-            if queryResult.worldFragment is not None:
-#                 pos = self.camera.getPosition()
-#                 self.camera.setPosition (pos.x, pos.y - queryResult.distance + 10.0, pos.z)
-#                 break
-                wf = queryResult.worldFragment
-                self.camera.setPosition(self.camera.getPosition().x,
-                    wf.singleIntersection.y + 10,
-                    self.camera.getPosition().z)
-                break               
-        return ret
-
-
-
-
-class CaelumSpeedFrameListener(TerrainFrameListener):
-    def __init__(self, renderWindow, camera, sceneManager, caelumSystem):
-        TerrainFrameListener.__init__(self, renderWindow, camera, sceneManager)
-
-        self.caelumSystem = caelumSystem
-        self.speedFactor = 0
-        self.paused = False
-        self.timeTillNextUpdate = 0
-        self.PostFiltering = False
+    def __init__(self, win, cam, app ):
+        sf.FrameListener.__init__(self, win, cam)
+        self.sceneManager = cam.getSceneManager()
+        self.window = win
+        self.camera = cam
+        self.paused=False
+        self.app = app
         
-        self.UpdateSpeedFactor(self.caelumSystem.getUniversalClock ().getTimeScale ())
-    
-    ## ------------------------------------------------------------------ 
-    ## our own function to set time scale dependent on paused state
-    ## saves a few if / else
-    ## ------------------------------------------------------------------ 
-#     def _setTimeScale(self, paused, t1, t2 ):
-#         t = t1
-#         if not paused: 
-#             t = t2
-#         self.caelumSystem.getUniversalClock().setTimeScale(t)
-    
+        # Pick components to create in the demo.
+        # You can comment any of those and it should still work
+        # Trying to disable one of these can be useful in finding problems.
+        componentMask = caelum.CaelumSystem.CaelumComponent (
+                caelum.CaelumSystem.CAELUM_COMPONENT_SUN |				
+                caelum.CaelumSystem.CAELUM_COMPONENT_MOON |
+                caelum.CaelumSystem.CAELUM_COMPONENT_SKY_DOME |
+                ## caelum.CaelumSystem.CAELUM_COMPONENT_IMAGE_STARFIELD |
+                caelum.CaelumSystem.CAELUM_COMPONENT_POINT_STARFIELD |
+                caelum.CaelumSystem.CAELUM_COMPONENT_CLOUDS |
+                0);
+        componentMask = caelum.CaelumSystem.CAELUM_COMPONENTS_DEFAULT
+
+        ## Initialise CaelumSystem.
+        self.mCaelumSystem = caelum.CaelumSystem (ogre.Root.getSingletonPtr(), self.sceneManager, componentMask)
+
+        ## Set time acceleration.
+        self.mCaelumSystem.getUniversalClock ().setTimeScale (512)
+
+        ## Register caelum as a listener.
+        self.app.renderWindow.addListener (self.mCaelumSystem)
+        ogre.Root.getSingletonPtr().addFrameListener (self.mCaelumSystem)
+
+        self.UpdateSpeedFactor(self.mCaelumSystem.getUniversalClock ().getTimeScale ())
+        self.timeTillNextUpdate = 0
+
     ## Update speed factors.
     def UpdateSpeedFactor(self, factor):
-        self.speedFactor = factor
+        self.speedFactor = factor;
         if self.paused:
-            t = 0
-        else:
-            t= self.speedFactor
-        self.caelumSystem.getUniversalClock ().setTimeScale (t)
-        ogre.ControllerManager.getSingleton().setTimeFactor (t)
+            self.mCaelumSystem.getUniversalClock ().setTimeScale (0)
+        else:            
+            self.mCaelumSystem.getUniversalClock ().setTimeScale (self.speedFactor)
+            
+    
+    def frameEnded(self, evt):
+        if not sf.FrameListener.frameEnded(self, evt):
+            return False
 
-    ## ------------------------------------------------------------------ 
-    def frameStarted(self, frameEvent):
-
-        # clamp to terrain
-        ret =  TerrainFrameListener.frameStarted(self, frameEvent)
-        print "from framestarted", ret
-        return ret
+        ## Stop key repeat for these keys.
         ## keyboard input
-        self.timeTillNextUpdate -= frameEvent.timeSinceLastFrame
+        self.timeTillNextUpdate -= evt.timeSinceLastFrame
         if (self.timeTillNextUpdate<= 0):
             if (self.Keyboard.isKeyDown (ois.KC_SPACE)):
                 self.timeTillNextUpdate = 1
@@ -113,187 +82,48 @@ class CaelumSpeedFrameListener(TerrainFrameListener):
             if (self.Keyboard.isKeyDown(ois.KC_Z)):
                 self.timeTillNextUpdate = 1
                 self.UpdateSpeedFactor(self.speedFactor * -1)
+        return True
 
-            if (self.Keyboard.isKeyDown(ois.KC_F1)):
-                self.timeTillNextUpdate = 1
-                if self.PostFiltering:
-                    self.PostFiltering = False
-                else:
-                    self.PostFiltering = True
-                ogre.CompositorManager.getSingleton().setCompositorEnabled (self.window.getViewport (0), "Bloom", self.PostFiltering)
-
-        ## Do some additional update. These should be removed from here;
-        ## CaelumSystem should do all required updates.
-
-        ## Update the haze sun light position
-        if (self.caelumSystem.getSun ()):
-            mat = ogre.MaterialManager.getSingleton ().getByName ("CaelumDemoTerrain")
-            if mat.getTechnique (0).getPass ("CaelumHaze"):
-                mat.getTechnique (0).getPass ("CaelumHaze").getVertexProgramParameters ().setNamedConstant ("sunDirection", 
-                                                                                        self.caelumSystem.getSun ().getLightDirection ())
-        return ret
-
-class TerrainApplication(sf.Application):
-
-    def __init__(self):
         
-        self.caelumSystem = None   ## Caelum system
-        self.caelumModel = None    ## Caelum model
-        self.spm = None
-        self.raySceneQuery = None
+class CaelumSampleApplication(sf.Application):
+    def _isPsycoEnabled(self):
+        return True
 
-        sf.Application.__init__(self)
+    def _createFrameListener ( self ):
+        self.FrameListener = CaelumSampleFrameListener (self.renderWindow, self.camera, self)
+        self.root.addFrameListener (self.FrameListener)
 
-
-    def __del__(self):
-
-        print "Remove Window Listener" 
-        self.renderWindow.removeListener(self.caelumSystem)
-        self.root.removeFrameListener(self.frameListener)
-        self.frameListener = None
-        print "deleted Listeners" 
-#         t = self.caelumSystem.getSun().setSunPositionModel(None)
-#         print "Sun is None", t
-#         t = self.caelumSystem.getSun().getSunPositionModel()
-#         print "Sun is None", t
-
-
- 
-        ## ====================================================================
-        ## This the one difference between the python and c++ demo. In c++ the
-        ## Sun dtor deletes the SunPositionalModel. However in createScene we
-        ## need to hold a reference to SunPositionModel (self.spm) other
-        ## seg faults when it goes out of scope. Doing so means that a seg fault
-        ## when C++ tries to delete the sun.EarthSunPositionModel. Temp workaround
-        ## comment out the following in Sun.cpp and handle deletion ouselves.
-        ## 
-        ##    /*
-        ##  if (mSunPositionModel)  {
-        ##      delete mSunPositionModel
-        ##      mSunPositionModel = 0
-        ##  }
-        ##    */
-        ## 
-        ## ====================================================================
-        if self.spm:
-            self.spm = 0
-        if self.caelumSystem:
-            self.caelumSystem.shutdown(False)
-            self.caelumSystem = None
-
-        del self.camera
-        del self.sceneManager
-        del self.frameListener
-        del self.root
-        del self.renderWindow
-
-
-    def _chooseSceneManager(self):
+    def _chooseSceneManager( self ):
         self.sceneManager = self.root.createSceneManager("TerrainSceneManager")
 
-    def _createCamera(self):
-        self.camera = self.sceneManager.createCamera('PlayerCam')
-        
+    def _createCamera( self ):
+        ## Create the camera
+        self.camera = self.sceneManager.createCamera("PlayerCam")
+
         ## Start the camera on a hill in the middle of the terrain
         ## looking towards Z+ (north).
         ## Sun should rise in the east(left) and set in the west(right).
-        self.camera.setPosition (ogre.Vector3(775, 100, 997))
-        self.camera.lookAt (ogre.Vector3(775, 100, 1000))
-        
-        ## Set camera clip distances. Its important to test with
+        self.camera.setPosition (ogre.Vector3 (775, 100, 997))
+        self.camera.lookAt (ogre.Vector3 (775, 100, 1000))
+
+        self.camera.setNearClipDistance(5)
+
+        ## Set camera clip distances. It's important to test with
         ## an infinite clip distance.
-        self.camera.setFarClipDistance(0);
-        self.camera.setNearClipDistance(5);
+        self.camera.setFarClipDistance(0)
+        ##mCamera.setFarClipDistance(10000);
+   
         
-        self.raySceneQuery = self.sceneManager.createRayQuery(ogre.Ray(self.camera.getPosition(), ogre.Vector3().NEGATIVE_UNIT_Y))
-        
-
-    def _createScene(self):
-        sceneManager = self.sceneManager
-        camera = self.camera
-        clouds = True
-        skyDome = True
-        
-        ## a little hack to let me test this on a machine with a crap graphics card
-        capabilities = ogre.Root.getSingleton().getRenderSystem().capabilities
-        if not capabilities.hasCapability(ogre.RSC_VERTEX_PROGRAM) or\
-                    not capabilities.hasCapability(ogre.RSC_FRAGMENT_PROGRAM):
-            skyDome = False
-            clouds = False
-        else:
-            ogre.CompositorManager.getSingleton().addCompositor(self.renderWindow.getViewport(0),"Bloom")
-        camera.setNearClipDistance(0.01)
-        componentMask = caelum.CaelumSystem.CaelumComponent(
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_SKY_DOME | 
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_SUN |
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_MOON |
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_IMAGE_STARFIELD | 
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_POINT_STARFIELD |
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_CLOUDS |
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_PRECIPITATION |
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_SCREEN_SPACE_FOG |
-#                     caelum.CaelumSystem.CAELUM_COMPONENT_GROUND_FOG |
-
-#                     caelum.CaelumSystem.CAELUM_COMPONENTS_NONE |
-                    caelum.CaelumSystem.CAELUM_COMPONENTS_DEFAULT |
-#                     caelum.CaelumSystem.CAELUM_COMPONENTS_ALL |
-
- 
-                    0 )
-        print componentMask
-        ## Initialise Caelum
-        self.caelumSystem = caelum.CaelumSystem(self.root.getSingletonPtr(), self.sceneManager ,componentMask )
-                
-        self.caelumSystem.setManageSceneFog(True)
-        self.caelumSystem.setSceneFogDensityMultiplier(0.0015)
-        self.caelumSystem.setManageAmbientLight (True)
-        self.caelumSystem.setMinimumAmbientLight (ogre.ColourValue (0, 0, 0.5))
-         
-        ## This is how you switch the sun implementation.
-        ## This here is a no-op; but it's useful to test caelum doesn't crash when the second sun is created.
-        self.spheresun = caelum.SphereSun(self.sceneManager, self.caelumSystem.getCaelumCameraNode ())
-        self.caelumSystem.setSun (self.spheresun)
-
-        ## Setup sun options
-        if self.caelumSystem.getSun ():
-            self.caelumSystem.getSun ().setAmbientMultiplier (ogre.ColourValue(0.5, 0.5, 0.5))
-            self.caelumSystem.getSun ().setDiffuseMultiplier (ogre.ColourValue(3, 3, 2.7))
-            self.caelumSystem.getSun ().setSpecularMultiplier (ogre.ColourValue (5, 5, 5))
-            self.caelumSystem.getSun ().setAutoDisable (True)
-            self.caelumSystem.getSun ().setAutoDisableThreshold (0.1)
-
-        if (self.caelumSystem.getMoon ()):
-            self.caelumSystem.getMoon ().setAutoDisable (True)
-            self.caelumSystem.getMoon ().setAutoDisableThreshold (0.1)
-
-#         ## Setup cloud options.
-#         ## Tweak these settings to make the demo look pretty.
-        if self.caelumSystem.getCloudSystem ():
-         
-           self.caelumSystem.getCloudSystem ().createLayerAtHeight(2000)
-           self.caelumSystem.getCloudSystem ().createLayerAtHeight(5000)
-           
-           self.caelumSystem.getCloudSystem ().getLayer(0).setCloudSpeed(ogre.Vector2(0.000005, -0.000009))
-           self.caelumSystem.getCloudSystem ().getLayer(1).setCloudSpeed(ogre.Vector2(0.0000045, -0.0000085))
-           
-  
-        ## Register caelum to the render target
-        self.renderWindow.addListener (self.caelumSystem)
-        ogre.Root.getSingletonPtr().addFrameListener( self.caelumSystem )
-        
+    def _createScene ( self ):
         ## Put some terrain in the scene
         self.sceneManager.setWorldGeometry ("CaelumDemoTerrain.cfg")
-
-    def _createFrameListener(self):
-    #    pass
-    #    return
-        return
-        self.frameListener = CaelumSpeedFrameListener(self.renderWindow, self.camera,self.sceneManager, self.caelumSystem)
-#         self.root.addFrameListener(self.frameListener)
-
+        self.sceneManager.setAmbientLight ( ogre.ColourValue(0,0,0 ))
+        
+       
+        
 if __name__ == '__main__':
     try:
-        application = TerrainApplication()
+        application = CaelumSampleApplication()
         application.go()
 
     except ogre.OgreException, e:
