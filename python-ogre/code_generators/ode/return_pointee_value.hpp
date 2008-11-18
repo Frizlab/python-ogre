64729d7d6f936cac0a8da5a60ee3aaf234949ffe
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2008-11-18 07:52:54 +0000

    OgreOde and Ode updates for Linux
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@799 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/code_generators/ode/return_pointee_value.hpp
+++ python-ogre/code_generators/ode/return_pointee_value.hpp
@@ -1,3 +1,8 @@
+// Copyright 2004-2008 Roman Yakovenko.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef RETURN_POINTEE_VALUE_9_11_2006
 #define RETURN_POINTEE_VALUE_9_11_2006
 
@@ -25,7 +30,7 @@ namespace detail{
     };
 
     template <class R>
-    struct reference_existing_object_requires_a_pointer_return_type
+    struct return_pointee_value_requires_a_pointer_return_type
     # if defined(__GNUC__) && __GNUC__ >= 3 || defined(__EDG__)
     {}
     # endif
@@ -44,7 +49,7 @@ struct return_pointee_value
         typedef typename mpl::if_c<
             ok
             , to_python_indirect<T, detail::make_value_holder>
-            , detail::reference_existing_object_requires_a_pointer_return_type<T>
+            , detail::return_pointee_value_requires_a_pointer_return_type<T>
         >::type type;
     };
 };
