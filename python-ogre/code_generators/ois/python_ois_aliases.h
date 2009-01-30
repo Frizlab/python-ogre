2bf0d46940493562402a9e8a07258d9c90a00ce7
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2009-01-30 05:44:36 +0000

    Updates to OIS to make vector<bool> usable..
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@866 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/code_generators/ois/python_ois_aliases.h
+++ python-ogre/code_generators/ois/python_ois_aliases.h
@@ -5,5 +5,6 @@ typedef std::vector<OIS::Axis> AxisVector;
 //typedef OIS::SupportedEffectList SupportedEffectList;
 typedef std::multimap<OIS::Type,std::basic_string<char, std::char_traits<char>, std::allocator<char> >,std::less<OIS::Type>,std::allocator<std::pair<const OIS::Type, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >\
     multimapOISType;
-
+typedef std::vector<bool> stdVectorBool; // nicer name than _Bvector
+typedef std::_Vb_reference<unsigned int, int, std::vector<bool, std::allocator<bool> > > stdVectorBoolReference; //
 
