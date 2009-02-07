c754e5981fa3a47071c90a1117b544bd37eab1cd
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2009-02-07 00:32:54 +0000

    Fixed OIS issues on non windows platforms
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@874 503f2841-2733-4260-b5db-97e3c2726e36

--- code_generators/ois/python_ois_aliases.h
+++ code_generators/ois/python_ois_aliases.h
@@ -6,5 +6,7 @@ typedef std::vector<OIS::Axis> AxisVector;
 typedef std::multimap<OIS::Type,std::basic_string<char, std::char_traits<char>, std::allocator<char> >,std::less<OIS::Type>,std::allocator<std::pair<const OIS::Type, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >\
     multimapOISType;
 typedef std::vector<bool> stdVectorBool; // nicer name than _Bvector
-typedef std::_Vb_reference<unsigned int, int, std::vector<bool, std::allocator<bool> > > stdVectorBoolReference; //
 
+#if defined( __WIN32__ ) || defined( _WIN32 )
+    typedef std::_Vb_reference<unsigned int, int, std::vector<bool, std::allocator<bool> > > stdVectorBoolReference; //
+#endif
