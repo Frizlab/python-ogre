4c82db366a37ad79362cc0015bd3b70774afd259
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2009-02-08 05:56:00 +0000

    Adding unittest batch file and upload helper for SourceForge
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@881 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ scripts/unittestall.bat
@@ -0,0 +1,1 @@
+python unittest.py -d ogre  -d caelum -d cegui -d et -d hikari -d hydrax -d noise -d nxogre -d ogreal -d ogrebullet -d ogreforests -d OgreNewt -d ogreode -d ogrepcz -d particleuniverse -d PSSM -d qgui -d theora
