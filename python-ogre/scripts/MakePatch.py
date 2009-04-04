44e2ba1162af38058b07fc2a4c5c3d6b07948b14
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2009-04-04 11:14:22 +0000

    Added Python 2.6 files and updated a few scripts Removed Theora module from packages
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@942 503f2841-2733-4260-b5db-97e3c2726e36

---
+++ python-ogre/scripts/MakePatch.py
@@ -0,0 +1,123 @@
+from optparse import OptionParser
+import sys
+import os
+import time
+import hashlib
+import shutil
+    
+def samefile ( originalFile, latestFile):
+    if not os.path.exists( latestFile ):
+        return False
+    if not os.path.exists( originalFile ):
+        return False
+    inhash = hashlib.md5() 
+    inhash.update ( open ( originalFile ).read() )
+    outhash = hashlib.md5()
+    outhash.update ( open ( latestFile ).read() )
+    if inhash.digest() != outhash.digest():
+        return False    
+    return True
+                                
+def copyTree ( originalPath, latestPath, patchPath,
+                      badPaths=['.svn', 'tofix', 'tests', 'wxOgre', 'showcase_01', 'media_old', 'nxogre', 'navi', 'tilemanager', 'dshow'],
+                      badExtensions=['pyc','bak','log', 'ogre.cfg'],
+                      files = [], recursive=True ):
+    """ function to do a nice tree copy with file filtering and directory creation etc 
+    """
+    def makePath ( pathIn ):
+        if not os.path.exists (pathIn ):
+            os.makedirs ( pathIn )
+            
+    def copyFile ( fileName, originalPath, latestPath, patchPath ):
+        originalFile = os.path.join ( originalPath, fileName )
+        latestFile = os.path.join ( latestPath, fileName)
+        patchFile = os.path.join ( patchPath, fileName)
+        # we only copy the file if it's a different size and or date
+        if not samefile( originalFile ,latestFile ):
+            makePath (patchPath)
+            shutil.copyfile( latestFile, patchFile )
+            print "Copying ", latestFile, " to ", patchFile,
+            
+    def filterExtensions ( listin, extensions):
+        returnList=[]
+        for i in listin:
+            insertOK = True
+            for e in extensions:
+               if i.endswith (e):
+                  insertOK = False
+                  break
+            if insertOK:
+               returnList.append(i)
+                     
+#         for i in listin:
+#             e=i.split('.')
+#             if len(e) >0:
+#                 if e[-1] not in extensions:
+#                     returnList.append(i)
+        return returnList  
+        
+ 
+    ## Main code starts here
+    
+    if not recursive: # Ok so we only care about a single directory 
+        if files == []:
+            files =  os.listdir( latestPath )   # get files
+        if len( files ) > 0: 
+            makePath ( patchPath )   # make a path if need be
+            for fileName in files:
+                copyFile ( fileName, originalPath, latestPath, patchPath)  # copy the file
+                
+    else:   # need to do a recursive copy
+        level = 0
+        for currentDir, dirs, files in os.walk(latestPath):
+            # remember the initial directory so we can get the subdirs later
+            if not level:
+                rootPath= currentDir
+                subPath = ""
+                level += 1
+            else:
+                subPath = currentDir[ len (rootPath) + 1:] # extract the sub directory path
+            pathOK = True
+            for s in badPaths:
+               if s in subPath:
+                  pathOK=False
+                  break
+            if not pathOK:
+               continue                 
+            # filter the file names
+            files = filterExtensions ( files, badExtensions )
+            
+            # Make all the directories in the dest Tree (for this level)
+#             if len(files)>0:
+#                makePath  (os.path.join (  patchPath, subPath))
+#                print "makepath", patchPath, subPath
+#                print files
+#             for dirName in dirs:
+#                 makePath ( os.path.join (  patchPath, subPath, dirName ) )
+                
+            # finally do the copy                
+            for fileName in files:
+                copyFile ( fileName, os.path.join( originalPath, subPath), currentDir, os.path.join(patchPath, subPath ) )
+                    
+def parseInput():
+    """Handle command line input """
+    usage = "usage: %prog [options] original new dest"
+    parser = OptionParser(usage=usage, version="%prog 1.0")
+#     parser.add_option("-v", "--verbose", action="store_true", default=False,dest="verbose", help="Output all output to stdout rather then saving to the log file")
+#     parser.add_option("-l", "--logfilename",  default="log.out" ,dest="logfilename", help="Override the default log file name")
+
+    (options, args) = parser.parse_args()
+    return (options,args)
+
+if __name__ == '__main__':
+
+#     (options, args) = parseInput()
+#     if len(args) == 0 and not (options.compilecodeall or options.gencodeall):
+#         exit("The module to build wasn't specified.  Use -h for help")
+    directories = ['plugins', 'demos', 'packages_2.5']
+    original = "C:/PythonOgre845"
+    latest = "c:/development/python-ogre"
+    dest  = "c:/temp/patch"
+    for d in directories:
+      copyTree (os.path.join ( original, d), os.path.join(latest, d), os.path.join ( dest,d ) )
+      
--- python-ogre/scripts/UpdateDLLs.bat
+++ python-ogre/scripts/UpdateDLLs.bat
@@ -4,15 +4,16 @@ echo ====== Update DLL's ======
 echo 
 setlocal
 set root=c:\development
-set package=..\packages_2.5\ogre
+
+rem Support for 2 versions of Python...
+set package_2.5=..\packages_2.5\ogre
+set package_2.6=..\packages_2.6\ogre
+
 
 set ogre=%root%\ogre\samples\common\bin\release
-rem set ogre=c:\ogreSDK\bin\release
-
 set ogretools=%root%\ogre\tools\common\bin\release
 
 set cegui=%root%\cegui-0.6.2\bin
-rem set cegui=%ogre%
 
 set quick=%root%\quickgui\bin
 set opcode=%root%\OpCode\Release
@@ -24,8 +25,12 @@ set theora=%root%\ogrevideo\TheoraVideoPlugin\bin\Release
 set ffmpeg=%root%\python-ogre\thirdparty\extra
 set naviextra=%root%\navi\Dependencies\win32\awesomium\bin\release
 set navi=%root%\navi\navi\lib
-set boost=%root%\boost_1_38_0\bin.v2\libs\python\build\msvc-9.0\release\threading-multi
-set boostlib=%boost%\boost_python-vc90-mt-1_38.dll
+
+set boost_2.5=%root%\boost_1_38_0\bin.v2\libs\python_2.5\build\msvc-9.0\release\threading-multi
+set boost_2.6=%root%\boost_1_38_0\bin.v2\libs\python_2.6\build\msvc-9.0\release\threading-multi
+set boostlib_2.5=%boost_2.5%\boost_python-vc90-mt-1_38.dll
+set boostlib_2.6=%boost_2.6%\boost_python-vc90-mt-1_38.dll
+
 set particleuniverse=%root%\ogre\lib
 
 set ogreal=%root%\OgreAL\lib\Release
@@ -36,12 +41,16 @@ set ogg=%root%\ogg\win32\Dynamic_Release
 set openal=%root%\openal\redist
 
 rem Ogre
-copy /y %ogre%\ogremain.dll %package%\renderer\OGRE
-copy /y %ogre%\cg.dll %package%\renderer\OGRE
+copy /y %ogre%\ogremain.dll %package_2.5%\renderer\OGRE
+copy /y %ogre%\cg.dll %package_2.5%\renderer\OGRE
+copy /y %ogre%\ogremain.dll %package_2.6%\renderer\OGRE
+copy /y %ogre%\cg.dll %package_2.6%\renderer\OGRE
 
 copy /y %ogretools%\* ..\tools
 
-copy /y %boostlib% %package%\renderer\OGRE
+copy /y %boostlib_2.5% %package_2.5%\renderer\OGRE
+copy /y %boostlib_2.6% %package_2.6%\renderer\OGRE
+
 copy /y %ogre%\render*.dll ..\plugins
 copy /y %ogre%\plug*.dll ..\plugins
 
@@ -52,55 +61,94 @@ rem ParticleUniverse
 copy /y %particleuniverse%\ParticleUniverse.dll ..\plugins
 
 rem Physics
-copy /y %ogre%\ReferenceAppLayer.dll %package%\physics\OgreRefApp
-REM copy /y %opcode%\opcode.dll %package%\physics\OpCode
-copy /y %nxogre%\nxogre.dll %package%\physics\NxOgre
-copy /y %newton%\newton.dll %package%\physics\OgreNewt
-copy /y %boostlib% %package%\physics\ode
-copy /y %boostlib% %package%\physics\bullet
-copy /y %boostlib% %package%\physics\physx
+copy /y %ogre%\ReferenceAppLayer.dll %package_2.5%\physics\OgreRefApp
+copy /y %ogre%\ReferenceAppLayer.dll %package_2.6%\physics\OgreRefApp
 
+REM copy /y %opcode%\opcode.dll %package%\physics\OpCode
+copy /y %nxogre%\nxogre.dll %package_2.5%\physics\NxOgre
+copy /y %newton%\newton.dll %package_2.5%\physics\OgreNewt
+copy /y %boostlib_2.5% %package_2.5%\physics\ode
+copy /y %boostlib_2.5% %package_2.5%\physics\bullet
+copy /y %boostlib_2.5% %package_2.5%\physics\physx
+
+copy /y %nxogre%\nxogre.dll %package_2.6%\physics\NxOgre
+copy /y %newton%\newton.dll %package_2.6%\physics\OgreNewt
+copy /y %boostlib_2.6% %package_2.6%\physics\ode
+copy /y %boostlib_2.6% %package_2.6%\physics\bullet
+copy /y %boostlib_2.6% %package_2.6%\physics\physx
 
 rem OgreODE
-copy /y %ogreode%\lib\Release\OgreOde_Core.dll %package%\physics\OgreOde
-copy /y %ogreode%\loader\lib\Release\OgreOde_Loader.dll %package%\physics\OgreOde
-copy /y %ogreode%\prefab\lib\Release\OgreOde_Prefab.dll %package%\physics\OgreOde
+copy /y %ogreode%\lib\Release\OgreOde_Core.dll %package_2.5%\physics\OgreOde
+copy /y %ogreode%\loader\lib\Release\OgreOde_Loader.dll %package_2.5%\physics\OgreOde
+copy /y %ogreode%\prefab\lib\Release\OgreOde_Prefab.dll %package_2.5%\physics\OgreOde
+
+copy /y %ogreode%\lib\Release\OgreOde_Core.dll %package_2.6%\physics\OgreOde
+copy /y %ogreode%\loader\lib\Release\OgreOde_Loader.dll %package_2.6%\physics\OgreOde
+copy /y %ogreode%\prefab\lib\Release\OgreOde_Prefab.dll %package_2.6%\physics\OgreOde
+
 rem Physx
-copy /y %physx%\Nx*.dll %package%\physics\PhysX
-copy /y %physx%\Ph*.dll %package%\physics\PhysX
-copy /y %physx%\Nx*.dll %package%\physics\NxOgre_09
-rem Theora
+copy /y %physx%\Nx*.dll %package_2.5%\physics\PhysX
+copy /y %physx%\Ph*.dll %package_2.5%\physics\PhysX
+copy /y %physx%\Nx*.dll %package_2.5%\physics\NxOgre_09
+
+copy /y %physx%\Nx*.dll %package_2.6%\physics\PhysX
+copy /y %physx%\Ph*.dll %package_2.6%\physics\PhysX
+copy /y %physx%\Nx*.dll %package_2.6%\physics\NxOgre_09
+
+rem Theora/OgreVideo
 copy /y %theora%\Plugin_TheoraVideoSystem.dll ..\plugins
+
+
 REM copy /y %theora%\Plugin_TheoraVideoSystem.dll %package%\addons\theora
 
 rem FFMpeg
-copy /y %ffmpeg%\bin\*.dll %package%\addons\ogrevideoffmpeg
+copy /y %ffmpeg%\bin\*.dll %package_2.5%\addons\ogrevideoffmpeg
+copy /y %ffmpeg%\bin\*.dll %package_2.6%\addons\ogrevideoffmpeg
 
 rem  CEGUI
-copy /y %cegui%\CEGUIBase.dll %package%\gui\CEGUI
-copy /y %cegui%\CEGUIExpatParser.dll %package%\gui\CEGUI
-copy /y %cegui%\CEGUIFalagardWRBase.dll %package%\gui\CEGUI
-copy /y %cegui%\CEGUITinyXMLParser.dll %package%\gui\CEGUI
-copy /y %ogre%\OgreGUIRenderer.dll %package%\gui\CEGUI
+copy /y %cegui%\CEGUIBase.dll %package_2.5%\gui\CEGUI
+copy /y %cegui%\CEGUIExpatParser.dll %package_2.5%\gui\CEGUI
+copy /y %cegui%\CEGUIFalagardWRBase.dll %package_2.5%\gui\CEGUI
+copy /y %cegui%\CEGUITinyXMLParser.dll %package_2.5%\gui\CEGUI
+copy /y %ogre%\OgreGUIRenderer.dll %package_2.5%\gui\CEGUI
+
+copy /y %cegui%\CEGUIBase.dll %package_2.6%\gui\CEGUI
+copy /y %cegui%\CEGUIExpatParser.dll %package_2.6%\gui\CEGUI
+copy /y %cegui%\CEGUIFalagardWRBase.dll %package_2.6%\gui\CEGUI
+copy /y %cegui%\CEGUITinyXMLParser.dll %package_2.6%\gui\CEGUI
+copy /y %ogre%\OgreGUIRenderer.dll %package_2.6%\gui\CEGUI
+
 
 rem QuickGUI
 REM copy /y %quick%\QuickGUI.dll %package%\gui\QuickGui
 
 rem OgreAL
-copy /y %ogreal%\OgreAL.dll %package%\sound\OgreAL
-copy /y %vorbis%\VorbisEnc_Dynamic_Release\vorbisenc.dll %package%\sound\OgreAL
-copy /y %vorbis%\VorbisFile_Dynamic_Release\vorbisfile.dll %package%\sound\OgreAL
-copy /y %vorbis%\Vorbis_Dynamic_Release\vorbis.dll %package%\sound\OgreAL
-copy /y %ogg%\ogg.dll %package%\sound\OgreAL
+copy /y %ogreal%\OgreAL.dll %package_2.5%\sound\OgreAL
+copy /y %vorbis%\VorbisEnc_Dynamic_Release\vorbisenc.dll %package_2.5%\sound\OgreAL
+copy /y %vorbis%\VorbisFile_Dynamic_Release\vorbisfile.dll %package_2.5%\sound\OgreAL
+copy /y %vorbis%\Vorbis_Dynamic_Release\vorbis.dll %package_2.5%\sound\OgreAL
+copy /y %ogg%\ogg.dll %package_2.5%\sound\OgreAL
+
+copy /y %ogreal%\OgreAL.dll %package_2.6%\sound\OgreAL
+copy /y %vorbis%\VorbisEnc_Dynamic_Release\vorbisenc.dll %package_2.6%\sound\OgreAL
+copy /y %vorbis%\VorbisFile_Dynamic_Release\vorbisfile.dll %package_2.6%\sound\OgreAL
+copy /y %vorbis%\Vorbis_Dynamic_Release\vorbis.dll %package_2.6%\sound\OgreAL
+copy /y %ogg%\ogg.dll %package_2.6%\sound\OgreAL
+
 
 REM copy /y %freealut%\alut.dll %package%\sound\OgreAL
-copy /y %openal%\OpenAL32.dll %package%\sound\OgreAL
-copy /y %openal%\wrap_oal.dll %package%\sound\OgreAL
+copy /y %openal%\OpenAL32.dll %package_2.5%\sound\OgreAL
+copy /y %openal%\wrap_oal.dll %package_2.5%\sound\OgreAL
+copy /y %openal%\OpenAL32.dll %package_2.6%\sound\OgreAL
+copy /y %openal%\wrap_oal.dll %package_2.6%\sound\OgreAL
 
 rem navi
-copy /y %navi%\*.dll %package%\gui\navi
-copy /y %naviextra%\*.dll %package%\gui\navi
+copy /y %navi%\*.dll %package_2.5%\gui\navi
+copy /y %naviextra%\*.dll %package_2.5%\gui\navi
+copy /y %navi%\*.dll %package_2.6%\gui\navi
+copy /y %naviextra%\*.dll %package_2.6%\gui\navi
 
 
 rem plib
-copy /y %boostlib% %package%\addons\plib
+copy /y %boostlib% %package_2.5%\addons\plib
+copy /y %boostlib% %package_2.6%\addons\plib
