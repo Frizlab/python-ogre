0f13664f3cbf66fb104eb5868ca0718c18be518e
mithro <mithro@503f2841-2733-4260-b5db-97e3c2726e36>
2009-05-19 07:40:24 +0000

    Update to the maketar stuff for packaging for the demos.
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk@976 503f2841-2733-4260-b5db-97e3c2726e36

--- python-ogre/scripts/build-maketar.sh
+++ python-ogre/scripts/build-maketar.sh
@@ -5,18 +5,19 @@ if [ ! -e python-ogre-pristine ]; then
   exit 1
 fi
 
-CHANGELOG_DATE=`date +'%a, %d %b %Y %H:%m:%S %z'`
-
 rm -rvf python-ogre
 
+CHANGELOG_DATE=`date +'%a, %d %b %Y %H:%m:%S %z'`
 cd python-ogre-pristine
 svn update
-
 CHANGELOG_VERSION=`svnversion`
-
 cd ..
-cp -rvf python-ogre-pristine/ python-ogre/
 
+##########################################################################
+# Create the python-ogre package
+##########################################################################
+
+cp -rvf python-ogre-pristine/ python-ogre/
 rm -rvf \
 	python-ogre/boost								\
 	python-ogre/debs								\
@@ -30,7 +31,7 @@ rm -rvf \
 	python-ogre/ThirdParty/dshow					\
 	python-ogre/ThirdParty/History					\
 	python-ogre/ThirdParty/vcredist_x86.exe			\
-	python-ogre/ThirdParty/dxwebsetup.exe \
+	python-ogre/ThirdParty/dxwebsetup.exe			\
 	python-ogre/convertonew.sed						\
 	python-ogre/convert.py							\
 	python-ogre/convert.sed							\
@@ -39,8 +40,7 @@ rm -rvf \
 	python-ogre/installWarning.rtf					\
 	python-ogre/makehelp.bat						\
 	python-ogre/config.ini							\
-	python-ogre/PythonOgreInstallCreator.iss		\
-
+	python-ogre/PythonOgreInstallCreator.iss
 
 find python-ogre -name .svn | xargs rm -rf
 
@@ -54,7 +54,6 @@ cp python-ogre-pristine/debian/control ./debian.control
 cp python-ogre-pristine/debian/dsc ./python-ogre.dsc
 
 # Update the changelog with the subversion information.
-echo "Updating the changelog to $CHANGELOG_VERSION"
 cat > ./debian.changelog <<EOF
 python-ogre (1.6.0+$CHANGELOG_VERSION-0) unstable; urgency=low
 
@@ -64,4 +63,50 @@ python-ogre (1.6.0+$CHANGELOG_VERSION-0) unstable; urgency=low
 
 EOF
 cat python-ogre-pristine/debian/changelog >> ./debian.changelog
+
+##########################################################################
+# Create the python-ogre-demos package package
+##########################################################################
+rm -rf python-ogre-demos
+mkdir python-ogre-demos
+
+cp -rvf python-ogre-pristine/demos/* python-ogre-demos/
+# Clean up any svn directories
+find python-ogre-demos -name .svn | xargs rm -rf
+# Remove any plugins.cfg files
+find python-ogre-demos -name plugins.cfg | xargs rm
+# Remove any yet to work demos
+find python-ogre-demos -name tofix | xargs rm -rf
+# Remove windows only demos
+find python-ogre-demos -name dshow | xargs rm -rf
+# Remove any log files
+find python-ogre-demos -name \*.log | xargs rm -rf
+
+cd python-ogre-demos
+python ../python-ogre-pristine/scripts/python-cleanup.py
+find -name \*.py[co] | xargs rm
+cd ..
+
+# Setup the UnitTest scripts
+cp python-ogre-pristine/scripts/UnitTest.py python-ogre-demos/UnitTest.py
+echo "mkdir /tmp/python-ogre-demos" > python-ogre-demos/UnitTest.sh
+echo "python UnitTest.py -p "." -s /tmp/python-ogre-demos \$@" >> python-ogre-demos/UnitTest.sh
+chmod a+x python-ogre-demos/UnitTest.sh
+
+mkdir -p ../python-ogre-demos
+tar -czvf ../python-ogre-demos/python-ogre-demos.tar.gz python-ogre-demos
+cp python-ogre-pristine/debs/python-ogre-demos/rules ../python-ogre-demos/debian.rules
+cp python-ogre-pristine/debs/python-ogre-demos/control ../python-ogre-demos/debian.control
+cp python-ogre-pristine/debs/python-ogre-demos/dsc ../python-ogre-demos/python-ogre-demos.dsc
+
+# Update the changelog with the subversion information.
+cat > ../python-ogre-demos/debian.changelog <<EOF
+python-ogre-demos (1.6.0+$CHANGELOG_VERSION-0) unstable; urgency=low
+
+  * Updating for Subversion revision $CHANGELOG_VERSION.
+
+ -- Tim 'Mithro' Ansell <mithro@mithis.com>  $CHANGELOG_DATE
+
+EOF
+cat python-ogre-pristine/debs/python-ogre-demos/changelog >> ../python-ogre-demos/debian.changelog
 
