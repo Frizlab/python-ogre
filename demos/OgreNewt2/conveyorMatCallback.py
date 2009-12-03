02676628a4af027edcc9b6d231d6f9c41e559a92
andy_miller <andy_miller@503f2841-2733-4260-b5db-97e3c2726e36>
2009-12-03 06:02:33 +0000

    OgreNewt2 demo03 working (collision callbacks)
    git-svn-id: https://svn.code.sf.net/p/python-ogre/code/trunk/python-ogre@1034 503f2841-2733-4260-b5db-97e3c2726e36

--- demos/OgreNewt2/conveyorMatCallback.py
+++ demos/OgreNewt2/conveyorMatCallback.py
@@ -10,23 +10,41 @@ class conveyorMatCallback ( OgreNewt.ContactCallback ):
         self.ConveyorID = conveyorID
     
             
-    def userProcess(self):
-        ## first, find which body represents the conveyor belt!
-        if (self.m_body0.getType() == self.ConveyorID):
-            belt = self.m_body0.getUserData()
-            object = self.m_body1
-    
-        if (self.m_body1.getType() == self.ConveyorID):
-            belt = self.m_body1.getUserData()
-            object = self.m_body0
+    def contactsProcess( self, contactJoint, timeStep,  threadIndex ):
+        body0 = contactJoint.getBody0()
+        body1 = contactJoint.getBody1()
+        belt = None
+        
+        if (body0.getType() == self.ConveyorID):
+            belt = body0.getUserData().getData()[1] # we know it's a python object
+            object = body1
+        if (body1.getType() == self.ConveyorID):
+            belt = body1.getUserData().getData()[1]
+            object = body0
         if not belt:
-            return 0
+            return
         ## okay, found the belt... let's adjust the collision based on this.
         thedir = belt.getGlobalDir()
+        count = contactJoint.getContactCount()
+        contact = contactJoint.getFirstContact()
+        for x in range (count):
+            contact.rotateTangentDirections( thedir )
+            contactPos = Ogre.Vector3()
+            contactNorm = Ogre.Vector3()
+            contact.getPositionAndNormal(contactPos, contactNorm)
+            objectPos = Ogre.Vector3()
+            objectOri = Ogre.Quaternion()
+            object.getPositionOrientation(objectPos, objectOri)
+            objectContactPointVel = object.getVelocity() + (contactPos - objectPos)*object.getOmega()
+            result_accel = belt.getSpeed() - thedir.dotProduct(objectContactPointVel)
+            result_accel *= 10; # looks nicer
+            contact.setTangentAcceleration( result_accel, 0 )
+            if x < (count - 1): # don't call getNext on last pass as causes crash
+                contact = contact.getNext()
+        return
         
-        self.rotateTangentDirections( thedir )
-        result_accel = (thedir * belt.Speed) - object.getVelocity()
+    def onAABBOverlap( self,  body0, body1, threadIndex ):
+        if( body0.getType() == self.ConveyorID or body1.getType() == self.ConveyorID ):
+            return 1
+        return 0
         
-        self.setContactTangentAcceleration( result_accel.length(), 0 )
-    
-        return 1
